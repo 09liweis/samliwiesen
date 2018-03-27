4e41f40a4b1ae831d88924d2e6d70c51c00e5fe7
samliweisen <liweisen007@gmail.com>
2018-03-27 13:59:31 +0000

    add mongode url

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ port = process.env.PORT || 3000;
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/portfolio'); 
+// mongoose.connect('mongodb://localhost/portfolio');
+mongoose.connect('mongodb://heroku_6njptcbp:dg8h3o8v9dpjk1osignqn3ibel@ds125489.mlab.com:25489/heroku_6njptcbp');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
