c4054680b2d0ad583b69daa5739e3cf66aeb3683
samliweisen <liweisen007@gmail.com>
2018-04-16 02:25:16 +0000

    add property to Transaction schema

--- models/transaction.js
+++ models/transaction.js
@@ -2,5 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TransactionSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: 'Title is required for transaction'
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    update_at: {
+        type: Date,
+        default: Date.now
+    }
 });
