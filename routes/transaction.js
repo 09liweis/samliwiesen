613f796bfc6c4a9616e923c4e13a9d6746a530b8
samliweisen <liweisen007@gmail.com>
2018-04-17 18:08:35 +0000

    add route to get list of transaction

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,2 +1,7 @@
 var express = require('express');
 var router = express.Router();
+
+const TransactionController = require('../controllers/transaction.js');
+
+router.route('/')
+.get(TransactionController.transaction_list);
