7d456a31d4faf2cfee831ccf711d01d0d8f17e3e
samliweisen <liweisen007@gmail.com>
2018-05-01 20:26:58 +0000

    add route for transaction update

--- routes/transaction.js
+++ routes/transaction.js
@@ -7,4 +7,7 @@ router.route('/')
 .get(TransactionController.transaction_list)
 .post(TransactionController.transaction_new);
 
+router.route('/:id')
+.put(TransactionController.transaction_update);
+
 module.exports = router;
