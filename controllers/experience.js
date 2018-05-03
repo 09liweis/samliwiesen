ff7da75680977586e42f821c9f7742bf05ccc230
samliweisen <liweisen007@gmail.com>
2018-05-03 20:17:22 +0000

    add create new experience function

--- controllers/experience.js
+++ controllers/experience.js
@@ -7,6 +7,14 @@ exports.experience_list = function(req, res) {
     });
 };
 
+exports.experience_new = function(req, res) {
+    const newExperience = new Experience(req.body);
+    newExperience.save(function(err, experience) {
+        handleError(res, err);
+        res.json(experience);
+    });
+};
+
 function handleError(res, err) {
     if (err) {
         res.send(err);
