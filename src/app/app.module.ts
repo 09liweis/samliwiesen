bcdce59180e0b57f2b8ba1a0a6d4b702b0ed5399
samliweisen <liweisen007@gmail.com>
2018-05-09 19:52:27 +0000

    import httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import {MatCardModule, MatGridListModule} from '@angular/material';
@@ -16,6 +17,7 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
     MatCardModule,
     MatGridListModule,
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: '',
