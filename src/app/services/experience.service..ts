c9d2dc129200256bd4d6bc3550e202a97038b484
samliweisen <liweisen007@gmail.com>
2018-05-10 15:38:28 +0000

    add header to experience service

--- src/app/services/experience.service..ts
+++ src/app/services/experience.service..ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/Rx';
 
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
