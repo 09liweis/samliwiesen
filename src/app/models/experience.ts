bfd5f57ffbf7baca374af0481b2397d1f9de146b
samliweisen <liweisen007@gmail.com>
2018-05-11 03:17:32 +0000

    add experience class

---
+++ src/app/models/experience.ts
@@ -0,0 +1,8 @@
+export class Experience {
+    _id: string;
+    title: string;
+    company: string;
+    start_date: string;
+    end_date: string;
+    duty: Array<string>;
+}
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/Rx';
 
+import { Experience } from '../models/experience';
+
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
