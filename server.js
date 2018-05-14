1c5febbfcb28ce7a8a41d565e79dd42eac889ecd
samliweisen <liweisen007@gmail.com>
2018-05-14 14:53:52 +0000

    remove comment out code

--- server.js
+++ server.js
@@ -36,12 +36,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-// app.use(express.static(path.join(__dirname) + '/dist'));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
-
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
