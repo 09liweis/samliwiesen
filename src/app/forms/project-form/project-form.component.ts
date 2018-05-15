952c00570b5ebe5eb7f4b749d2ef8c5187c51aac
samliweisen <liweisen007@gmail.com>
2018-05-15 17:07:50 +0000

    initial empty project and git status

--- src/app/forms/project-form/project-form.component.ts
+++ src/app/forms/project-form/project-form.component.ts
@@ -10,13 +10,36 @@ import { Project } from '../../models/project';
   styleUrls: ['./project-form.component.scss']
 })
 export class ProjectFormComponent implements OnInit {
-
+  public project: Project = {
+    _id: '',
+    title: '',
+    description: '',
+    link: '',
+    features: ['']
+  }
   constructor(
     private projectService: ProjectService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+  }
+  
+  addFeature() {
+    let features = this.project.features;
+    features.push('');
+    this.project.features = features;
+  }
+  updateFeature(event: any, index) {
+    let features = this.project.features;
+    features[index] = event.target.value;
+    this.project.features = features;
+  }
+  
+  submit() {
+    this.projectService.submit(this.project).subscribe(ex => {
+      
+    });
   }
 
 }
