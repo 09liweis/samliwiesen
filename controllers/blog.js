4dbc00d18de768ec087174c81c4ef0c66aa62a2b
samliweisen <liweisen007@gmail.com>
2018-05-28 13:22:08 +0000

    add blog list function

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,1 +1,7 @@
 const Blog = require('../models/blog');
+
+exports.blog_list = function(req, res) {
+    Blog.find({}).sort('-created_at').exec(function(err, blogs) {
+        res.json(blogs);
+    });
+};
