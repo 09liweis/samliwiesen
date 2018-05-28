3c0cce7b25ef0385aa6c0ea6f9a9c674c0e63989
samliweisen <liweisen007@gmail.com>
2018-05-28 15:44:15 +0000

    add get list get detail submit function in blog service

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -3,10 +3,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 
+import { Blog } from '../models/blog';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  private api = 'https://samliweisen.herokuapp.com/api/blogs/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getList(): Observable<any> {
+    return this.http.get(this.api);
+  }
+  
+  getDetail(id: string): Observable<any> {
+    return this.http.get(this.api + id);
+  }
+  
+  submit(blog: Blog): Observable<any> {
+    if (blog._id != '') {
+      return this.http.put(this.api + blog._id, blog, httpOptions);
+    } else {
+      return this.http.post(this.api, blog, httpOptions);
+    }
+    }
+  }
 }
