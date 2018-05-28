9154da16dcb60c5e61d3742bb4b2580be81d285c
samliweisen <liweisen007@gmail.com>
2018-05-28 13:16:08 +0000

    fill up blog model

--- models/blog.js
+++ models/blog.js
@@ -1,1 +1,29 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+    title: String,
+    url: String,
+    content: String,
+    image: String,
+    category: String,
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    update_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+BlogSchema.pre('save', function(next) {
+    const currentDate = new Date();
+    this.update_at = currentDate;
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
