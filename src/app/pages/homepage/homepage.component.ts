1fc95baea4c4440aabdbb6a2417b0556206f4e53
samliweisen <liweisen007@gmail.com>
2018-05-29 13:33:52 +0000

    implement blog number on homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProjectService } from '../../services/project.service';
 import { ExperienceService } from '../../services/experience.service';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-homepage',
@@ -11,10 +12,12 @@ import { ExperienceService } from '../../services/experience.service';
 export class HomepageComponent implements OnInit {
   public experiences;
   public projects;
+  public blogs;
 
   constructor(
     private projectService: ProjectService,
-    private experienceService: ExperienceService
+    private experienceService: ExperienceService,
+    private blogService: BlogService
   ) { }
 
   ngOnInit() {
@@ -24,6 +27,10 @@ export class HomepageComponent implements OnInit {
     
     this.projectService.getList().subscribe(projs => {
       this.projects = projs;
+    });
+    
+    this.blogService.getList().subscribe(blogs => {
+      this.blogs = blogs;
     })
   }
 }
