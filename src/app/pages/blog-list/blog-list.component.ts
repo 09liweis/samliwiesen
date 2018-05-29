4eb719d38a01a69cddee23ca0aff8bdfafc53fa3
samliweisen <liweisen007@gmail.com>
2018-05-29 13:30:38 +0000

    implement blog list component

--- src/app/pages/blog-list/blog-list.component.ts
+++ src/app/pages/blog-list/blog-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogService } from '../../services/blog.service';
+
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.scss']
 })
 export class BlogListComponent implements OnInit {
+  public blogs;
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
   ngOnInit() {
+    this.blogService.getList().subscribe(blogs => {
+      this.blogs = blogs;
+    })
   }
 
 }
