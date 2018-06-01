0b3f42abd4eb5262f39d8cffa0060d4996b3d42d
samliweisen <liweisen007@gmail.com>
2018-06-01 14:48:19 +0000

    add router for place details

--- routes/place.js
+++ routes/place.js
@@ -6,4 +6,7 @@ const PlaceController = require('../controllers/place.js');
 router.route('/')
 .get(PlaceController.place_list);
 
+router.route('/:id')
+.get(PlaceController.place_detail);
+
 module.exports = router;
