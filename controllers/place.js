f514de5aefa65a696add330b9128ec7b3b013af9
samliweisen <liweisen007@gmail.com>
2018-06-01 15:13:06 +0000

    implement place detail page

--- controllers/place.js
+++ controllers/place.js
@@ -9,5 +9,10 @@ exports.place_list = function(req, res) {
 
 exports.place_detail = async function(req, res) {
     const p = await Place.findById(req.params.id);
-    
+    const transactions = await Transaction.find({place: p._id});
+    const result = {
+        place: p,
+        transactions: transactions
+    };
+    res.json(result);
 };
