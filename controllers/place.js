5e0a15007caff9a8bb7dac0f438d4364b5d6749b
samliweisen <liweisen007@gmail.com>
2018-06-01 14:53:27 +0000

    include Transaction in place controller

--- controllers/place.js
+++ controllers/place.js
@@ -1,4 +1,5 @@
 var Place = require('../models/place');
+var Transaction = require('../models/transaction');
 
 exports.place_list = function(req, res) {
     Place.find({}).exec(function(err, places) {
@@ -6,6 +7,7 @@ exports.place_list = function(req, res) {
     });
 };
 
-exports.place_detail = function(req, res) {
+exports.place_detail = async function(req, res) {
+    const p = await Place.findById(req.params.id);
     
 };
