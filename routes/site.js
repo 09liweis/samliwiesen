710d4162baa3f22d4ffb34c8fa7d989b16e75fca
samliweisen <liweisen007@gmail.com>
2018-08-22 17:46:21 +0000

    fix site router

--- routes/site.js
+++ routes/site.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const SiteController = require('../controllers/site.js');
 
 router.route('/:id')
-.get(SiteController.todo_detail)
-.put(SiteController.todo_update);
+.get(SiteController.site_detail)
+.put(SiteController.site_update);
 
 module.exports = router;
