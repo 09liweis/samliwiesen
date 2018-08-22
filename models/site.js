b717ec31a700ad18d1965a5d0768968fe174a108
samliweisen <liweisen007@gmail.com>
2018-08-22 15:26:29 +0000

    implement site model

---
+++ models/site.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SiteSchema = new Schema({
+    name: {
+        type: String
+    },
+    visit: {
+        type: Number,
+        default: 0
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    update_at: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+SiteSchema.pre('save', function(next) {
+    const currentDate = new Date();
+    this.update_at = currentDate;
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
+});
+
+module.exports = mongoose.model('Site', SiteSchema);
