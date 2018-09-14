cf8bfabeb92da8f4ca81c59ef02fdf3695f5f3f2
samliweisen <liweisen007@gmail.com>
2018-09-14 16:19:45 +0000

    add category filter for transaction

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -2,7 +2,12 @@ var Transaction = require('../models/transaction');
 var Place = require('../models/place');
 
 exports.transaction_list = function(req, res) {
-    Transaction.find({}, '_id title price date category').populate('place', '_id name address lat lng').sort('-date').exec(function(err, transactions) {
+    let filter = {};
+    const category = req.query.category;
+    if (category) {
+        filter.category = category;
+    }
+    Transaction.find(filter, '_id title price date category').populate('place', '_id name address lat lng').sort('-date').exec(function(err, transactions) {
         handleError(res, err);
         res.json(transactions);
     });
