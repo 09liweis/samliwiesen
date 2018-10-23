871f69339ae56450b518fbbd614a77403b304801
samliweisen <liweisen007@gmail.com>
2018-10-23 18:38:56 +0000

    add comment

---
+++ server/routes/comment.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+const Comment = require('../models/comment');
+
+router.get('/', function(req, res, next) {
+    Comment.find({}).exec((err, comments) => {
+        if (err) throw err;
+        res.send(comments);
+    });
+});
+
+router.post('/', function(req, res, next) {
+
+    const name = req.body.name;
+    const content = req.body.content;
+    const comment = Comment({
+        name: name,
+        content: content
+    });
+  
+    comment.save(function(err) {
+        if (err) throw err;
+        res.send(comment);
+    });
+});
+
+module.exports = router;
