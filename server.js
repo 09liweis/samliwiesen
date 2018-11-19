87567831ead7f9445e640eaa757a6e49c78c6c62
samliweisen <liweisen007@gmail.com>
2018-11-19 15:48:57 +0000

    update resume url

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname) + '/dist'));
 
-app.get('/', (req, res) => {
+app.get('/resume', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/resume.html'));
 });
 
