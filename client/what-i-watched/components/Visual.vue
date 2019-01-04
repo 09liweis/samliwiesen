4c72303d17add09e287d8868b16694c32c443d9e
samliweisen <liweisen007@gmail.com>
2019-01-04 16:45:23 +0000

    add methods to Visual component

---
+++ client/what-i-watched/components/Visual.vue
@@ -0,0 +1,65 @@
+<template>
+    <mu-card>
+        <mu-card-media title="" subTitle="">
+            <span class="visual__progress-episodes">{{v.current_episode}} / {{v.episodes}}</span>
+            <span class="visual__status">{{getStatus(v)}}</span>
+            <img class="visual__poster" :src="v.poster" />
+        </mu-card-media>
+        <mu-card-actions>
+            <div class="visual__titles">
+                <h3 class="visual__title">{{v.original_title ? v.original_title : v.title}}</h3>
+            </div>
+            <div class="visual__ratings">
+                <a class="visual__rating link" v-bind:href="getLink(v, 'douban')" target="_blank">
+                    <img class="visual__rating icon" src="https://img3.doubanio.com/f/talion/2f3c0bc0f35b031d4535fd993ae3936f4e40e6c8/pics/icon/dou32.png" alt="douban icon" />
+                    <span class="visual__rating">{{v.douban_rating}}</span>
+                </a>
+                <a class="visual__rating link" v-if="v.imdb_id" v-bind:href="getLink(v, 'imdb')" target="_blank">
+                    <img class="visual__rating icon" src="https://a4.mzstatic.com/us/r30/Purple71/v4/eb/6a/9d/eb6a9d94-4631-194c-3e24-852a06dc4ced/icon175x175.jpeg" alt="imdb icon" />
+                    <span class="visual__rating">{{v.imdb_rating}}</span>
+                </a>
+                <a v-if="v.rotten_rating">
+                    <img class="visual__rating icon" src="https://vignette.wikia.nocookie.net/greatest-movies/images/1/16/Rotten_Tomatoes_fresh_rating_icon.png/revision/latest?cb=20170918174417" alt="Rotten Tomatoes Icon" />
+                    <span class="visual__rating">{{v.rotten_rating}}</span>
+                </a>
+            </div>
+            <div class="visual__progress">
+                <mu-linear-progress mode="determinate" :value="getProgress(v)"/>
+            </div>
+            <div class="visual__action">
+                <router-link :to="{ name: 'edit', params: { id: v.id }}">Edit</router-link>
+                <a class="visual__increaseepisode" v-if="v.episodes != v.current_episode" v-on:click="increaseEpisode(v.id)">+ 1 ep</a>
+            </div>
+        </mu-card-actions>
+    </mu-card>
+</template>
+<script>
+export default {
+    props: ['v'],
+    methods: {
+        increaseEpisode(id) {
+            this.$http.get(this.$store.state.api.increaseEpisode + '?id=' + id).then(res => {
+                // this.getVisuals();
+            });
+        },
+        getStatus(v) {
+            if (v.current_episode == v.episodes) {
+                return 'Done';
+            } else if (v.current_episode == 0) {
+                return 'Not Started';
+            } else if (v.current_episode < v.episodes) {
+                return 'In Progress';
+            }
+        },
+        getProgress(v) {
+            if (v.current_episode == v.episodes) {
+                return 100;
+            }
+            return (v.current_episode/v.episodes) * 100;
+        },
+    }
+};
+</script>
+<style scoped>
+
+</style>
