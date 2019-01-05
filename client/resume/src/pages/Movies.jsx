6dc165589a97e85d14ede431de0daeb75be2d368
samliweisen <liweisen007@gmail.com>
2019-01-05 20:37:45 +0000

    remove movie style component

--- client/resume/src/pages/Movies.jsx
+++ client/resume/src/pages/Movies.jsx
@@ -3,47 +3,7 @@ import axios from 'axios';
 
 import styled from 'styled-components';
 
-export const Status = styled.div`
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    padding: 3px 7px;
-    border-radius: 10px;
-    color: #FFFFFF;
-    min-width: 44px;
-    text-align: center;
-    &.done {
-        background-color: #3e8c3e;
-    }
-    &.in_progress {
-        background-color: #ffc107;
-    }
-    &.not_started {
-        background-color: red;
-    }
-`;
-export const VisualContainer = styled.div`
-    border-radius: 5px;
-    background-color: #FFFFFF;
-    box-shadow: 0 1px 6px rgba(0,0,0,.117647), 0 1px 4px rgba(0,0,0,.117647);
-    border-bottom: 5px solid transparent;
-    position: relative;
-`;
-export const Visual = styled.div`
-    padding: 0 10px;
-    width: 100%;
-    margin-bottom: 15px;
-    transition: 0.3s;
-    &:hover {
-        transform: translateY(-3%);
-    }
-    @media (min-width: 768px) {
-        width: 25%;
-    }
-    @media (min-width: 1024px) {
-        width: 20%;
-    }
-`;
+import Movie from '../components/Movie.jsx';
 
 import '../css/movies.css';
 
@@ -80,40 +40,9 @@ export default class Movies extends React.Component {
     }
     render() {
         const visuals = this.state.visuals.map((v) => {
-            let status;
-            if (v.current_episode == v.episodes) {
-                status = 'done';
-            }
-            if (v.current_episode < v.episodes) {
-                status = 'in_progress';
-                if (v.current_episode == 0) {
-                    status = 'not_started';
-                }
-            }
             // status += ' visual__status';
             return (
-                <Visual key={v.id}>
-                    <VisualContainer>
-                        <Status className={status}>{v.current_episode}/{v.episodes}</Status>
-                        <img className="visual__image" src={v.poster} />
-                        <div className="visual__detail">
-                            <div>{v.release_date}</div>
-                            <h3 className="visual__title">{v.title}</h3>
-                            <div className="visual__ratings">
-                                <a className="visual__rating" target="_blank" href={'https://movie.douban.com/subject/' + v.douban_id}>
-                                    <img className="visual__rating-icon" src="https://img3.doubanio.com/f/talion/2f3c0bc0f35b031d4535fd993ae3936f4e40e6c8/pics/icon/dou32.png" alt="" />
-                                    <span className="visual__rating-point">{v.douban_rating}</span>
-                                </a>
-                                {v.imdb_id ?
-                                <a className="visual__rating" target="_blank" href={'https://www.imdb.com/title/' + v.imdb_id}>
-                                    <img className="visual__rating-icon" src="https://cdn0.iconfinder.com/data/icons/social-media-2091/100/social-31-512.png" alt="" />
-                                    <span className="visual__rating-point">{v.imdb_rating}</span>
-                                </a>
-                                : null}
-                            </div>
-                        </div>
-                    </VisualContainer>
-                </Visual>
+                <Movie v={v} key={v.id} />
             );
         }
         );
