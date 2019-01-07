d9e4660599a90bb021c77bbcff0c10b05685d66b
samliweisen <liweisen007@gmail.com>
2019-01-07 23:49:29 +0000

    update state to props

--- client/resume/src/components/Transaction.jsx
+++ client/resume/src/components/Transaction.jsx
@@ -11,18 +11,18 @@ export default class Transaction extends React.Component {
         //const iconClass = 'transaction__icon fa fa-' + icons[t.category.split(' ').join('_')].icon;
         const iconClass = '';
         return (
-            <div className="transaction__item" key={t._id}>
+            <div className="transaction__item">
                 <div className={iconClass}></div>
                 <div className="transaction__info">
                     <div className="transaction__title">{t.title + ' - ' + t.category}</div>
                     <div className="transaction__date">{t.date}</div>
                 </div>
                 <div className={priceClass}>${Math.abs(t.price)}</div>
-                {this.state.admin ? 
-                <div onClick={this.handleUpdate.bind(this, t)}>Edit</div>
+                {this.props.admin ? 
+                <div onClick={this.props.handleUpdate.bind(this, t)}>Edit</div>
                 : null}
-                {this.state.admin ? 
-                <div className="transaction__delete fa fa-times" onClick={this.handleDelete.bind(this, t)}></div>
+                {this.props.admin ? 
+                <div className="transaction__delete fa fa-times" onClick={this.props.handleDelete.bind(this, t)}></div>
                 :null}
             </div>
         );
