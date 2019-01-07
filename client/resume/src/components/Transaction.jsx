a9cce3623a87215942c3c34358a528410b9b07c2
samliweisen <liweisen007@gmail.com>
2019-01-07 23:37:30 +0000

    add Transaction component

---
+++ client/resume/src/components/Transaction.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class Transaction extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        
+    }
+}
