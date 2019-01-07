95427cdd1995be0e42ff617b86c7d27a1b56150f
samliweisen <liweisen007@gmail.com>
2019-01-07 23:40:52 +0000

    add render function to Transaction component

--- client/resume/src/components/Transaction.jsx
+++ client/resume/src/components/Transaction.jsx
@@ -6,6 +6,25 @@ export default class Transaction extends React.Component {
         super(props);
     }
     render() {
-        
+        const t = this.props.t;
+        const priceClass = (t.price > 0) ? 'transaction__price debit' : 'transaction__price credit';
+        //const iconClass = 'transaction__icon fa fa-' + icons[t.category.split(' ').join('_')].icon;
+        const iconClass = '';
+        return (
+            <div className="transaction__item" key={t._id}>
+                <div className={iconClass}></div>
+                <div className="transaction__info">
+                    <div className="transaction__title">{t.title + ' - ' + t.category}</div>
+                    <div className="transaction__date">{t.date}</div>
+                </div>
+                <div className={priceClass}>${Math.abs(t.price)}</div>
+                {this.state.admin ? 
+                <div onClick={this.handleUpdate.bind(this, t)}>Edit</div>
+                : null}
+                {this.state.admin ? 
+                <div className="transaction__delete fa fa-times" onClick={this.handleDelete.bind(this, t)}></div>
+                :null}
+            </div>
+        );
     }
 }
