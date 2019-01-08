b2e778063513496918737ab475a22369dd30b859
samliweisen <liweisen007@gmail.com>
2019-01-08 23:19:07 +0000

    add movie page component

---
+++ client/resume/src/pages/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class Movie extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Hello Movie</h1>
+        );
+    }
+}
