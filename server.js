57c1602e0a16015698adc5bded44916a35ad814b
samliweisen <liweisen007@gmail.com>
2019-01-11 20:06:21 +0000

    add TODO

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname) + '/dist'));
 
+//TODO: try to fix root to display resume site
 app.get('/resume', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/resume.html'));
 });
