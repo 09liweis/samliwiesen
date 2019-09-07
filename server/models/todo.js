98996117ecd24e904841bc63818b603d5df3e533
samliweisen <liweisen007@gmail.com>
2019-09-07 19:55:29 +0000

    decrease tab to 2 space

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,40 +2,40 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
-    name: {
-        type: String,
-        required: 'Kindly enter the name of the task'
-    },
-    steps: [
-        {
-            name: String,
-            status: {
-                type: String,
-                default: 'pending'
-            }
-        }
-    ],
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    update_at: {
-        type: Date,
-        default: Date.now
-    },
-    status: {
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  steps: [
+    {
+      name: String,
+      status: {
         type: String,
         default: 'pending'
+      }
     }
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: String,
+    default: 'pending'
+  }
 });
 
 TodoSchema.pre('save', function(next) {
-    const currentDate = new Date();
-    this.update_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+  const currentDate = new Date();
+  this.update_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
