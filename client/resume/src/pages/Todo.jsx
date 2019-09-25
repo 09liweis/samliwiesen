e1a9e5601599c40c29c8575df3d923e6021c6d09
Sam Li <weisen.li@hotmail.com>
2019-09-25 12:44:51 -0400

    refactor todo.jsx

--- client/resume/src/pages/Todo.jsx
+++ client/resume/src/pages/Todo.jsx
@@ -49,137 +49,131 @@ export default class Todo extends React.Component {
 			}
 		});
 	}
-    updateTodo(todo) {
-        axios.put(this.state.api + todo._id, todo).then((res) => {
-            if (res.status == 200) {
-                this.getTodos();
-            }
-        });
-    }
-    handleKeyPress(todo, e) {
-        if (e.key === 'Enter') {
-            if (e.target.value == 'go to admin mode') {
-                window.localStorage.setItem('admin', true);
-                this.setState({
-                    admin: true,
-                    newTodo: {
-                        name: '',
-                        status: 'pending'
-                    },
-                });
-                return false;
-            }
-            if (e.target.value == 'exit admin mode') {
-                window.localStorage.setItem('admin', '');
-                this.setState({
-                    admin: false,
-                    newTodo: {
-                        name: '',
-                        status: 'pending'
-                    },
-                });
-                return false;
-            }
-            if (todo != 'add') {
-                this.updateTodo(todo);
-            } else {
-                this.submitTodo();
-            }
-        }
-    }
-    handleChange(e) {
-        this.setState({
-            newTodo: {
-                name: e.target.value,
-                status: 'pending'
-            }
-        });  
-    }
-    handleUpdate(idx, e) {
-        const newTodos = this.state.todos.map((todo, i) => {
-            if (i !== idx) {
-                return todo;
-            } else {
-                todo.name = e.target.value;
-                return todo;   
-            }
-        });
-        this.setState({todos: newTodos});
-    }
-    handleComplete(idx) {
-        let todos = this.state.todos;
-        let status = todos[idx].status;
-        todos[idx].status = status == 'pending' ? 'working' : 'done';
-        let todo = todos[idx];
-        axios.put(this.state.api + todo._id, todo).then((res) => {
-            if (res.status == 200) {
-                this.setState({
-                    todos: todos
-                });
-            }
-        });
-    }
-    handleRemove(idx, id) {
-        let todos = this.state.todos;
-        todos.splice(idx, 1);
-        axios.delete(this.state.api + id).then((res) => {
-            if (res.status == 200) {
-                this.setState({
-                    todos: todos
-                });
-            }
-        });
-    }
-    setFilter(filter) {
-        this.setState({
-            filter: filter
-        });
-    }
-    getStatus(todo) {
-        return 'todo ' + todo.status;
-    }
-    render() {
-        const {admin, todos, newTodo, loading, filter} = this.state;
-        let todosFilter = todos;
-        if (filter != 'all') {
-            todosFilter = todos.filter(todo => todo.status == filter);
-        }
-        const todoList = todosFilter.map((todo, idx) => 
-                        <CSSTransition key={todo._id} timeout={500} classNames="todoAnimation">
-                        <div className={this.getStatus(todo)}>
-                            {admin ?
-                            <input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} onKeyPress={this.handleKeyPress.bind(this, todo)} />
-                            :
-                                <div className="todo__title">{todo.name}</div>
-                            }
-                            {todo.status != 'done' && admin ?
-                            <div className="todo__actions">
-                                <div className="todo__complete" onClick={this.handleComplete.bind(this, idx)}>{todo.status == 'pending' ? 'Working' : 'Done'}</div>
-                                <div className="todo__remove" onClick={this.handleRemove.bind(this, idx, todo._id)}>Remove</div>
-                            </div>
-                            :null}
-                        </div>
-                        </CSSTransition>
-                    );
-        return (
-            <div className="container">
-                <h2 className="todos__title">Todo List</h2>
-                <div className="todos__container">
-                    <input placeholder="Add New Todo" id="todoName" value={newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this, 'add')} />
-                    <div className="todo__statics">
-                        <span className="done" onClick={this.setFilter.bind(this, 'done')}>Done</span>
-                        <span className="working" onClick={this.setFilter.bind(this, 'working')}>In Progress</span>
-                        <span className="pending" onClick={this.setFilter.bind(this, 'pending')}>Pending</span>
-                    </div>
-                    {loading ? 
-                    <div className="todos__loader"></div>
-                    :
-                    <TransitionGroup>
-                    {todoList}
-                    </TransitionGroup>
-                    }
-                </div>
-            </div>
-        );
-    }
+	updateTodo(todo) {
+		axios.put(this.state.api + todo._id, todo).then((res) => {
+			if (res.status == 200) {
+				this.getTodos();
+			}
+		});
+	}
+	handleKeyPress(todo, e) {
+		if (e.key === 'Enter') {
+			if (e.target.value == 'go to admin mode') {
+				window.localStorage.setItem('admin', true);
+				this.setState({
+					admin: true,
+					newTodo: {
+						name: '',
+						status: 'pending'
+					},
+				});
+				return false;
+			}
+			if (e.target.value == 'exit admin mode') {
+				window.localStorage.setItem('admin', '');
+				this.setState({
+					admin: false,
+					newTodo: {
+						name: '',
+						status: 'pending'
+					},
+				});
+				return false;
+			}
+			if (todo != 'add') {
+				this.updateTodo(todo);
+			} else {
+				this.submitTodo();
+			}
+		}
+	}
+	handleChange(e) {
+		this.setState({
+			newTodo: {
+				name: e.target.value,
+				status: 'pending'
+			}
+		});
+	}
+	handleUpdate(idx, e) {
+		const newTodos = this.state.todos.map((todo, i) => {
+			if (i !== idx) {
+				return todo;
+			} else {
+				todo.name = e.target.value;
+				return todo;
+			}
+		});
+		this.setState({todos: newTodos});
+	}
+	handleComplete(idx) {
+		let todos = this.state.todos;
+		let status = todos[idx].status;
+		todos[idx].status = status == 'pending' ? 'working' : 'done';
+		let todo = todos[idx];
+		axios.put(this.state.api + todo._id, todo).then((res) => {
+			if (res.status == 200) {
+				this.setState({todos});
+			}
+		});
+	}
+	handleRemove(idx, id) {
+		let todos = this.state.todos;
+		todos.splice(idx, 1);
+		axios.delete(this.state.api + id).then((res) => {
+			if (res.status == 200) {
+				this.setState({todos});
+			}
+		});
+	}
+	setFilter(filter) {
+		this.setState({filter});
+	}
+	getStatus(todo) {
+		return 'todo ' + todo.status;
+	}
+	render() {
+		const {admin, todos, newTodo, loading, filter} = this.state;
+		let todosFilter = todos;
+		if (filter != 'all') {
+			todosFilter = todos.filter(todo => todo.status == filter);
+		}
+		const todoList = todosFilter.map((todo, idx) => 
+			<CSSTransition key={todo._id} timeout={500} classNames="todoAnimation">
+				<div className={this.getStatus(todo)}>
+					{admin ?
+					<input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} onKeyPress={this.handleKeyPress.bind(this, todo)} />
+					:
+					<div className="todo__title">{todo.name}</div>
+					}
+					{todo.status != 'done' && admin ?
+					<div className="todo__actions">
+						<div className="todo__complete" onClick={this.handleComplete.bind(this, idx)}>{todo.status == 'pending' ? 'Working' : 'Done'}</div>
+						<div className="todo__remove" onClick={this.handleRemove.bind(this, idx, todo._id)}>Remove</div>
+					</div>
+					:null}
+				</div>
+			</CSSTransition>
+		);
+		return (
+			<div className="container">
+				<h2 className="todos__title">Todo List</h2>
+				<div className="todos__container">
+					<input placeholder="Add New Todo" id="todoName" value={newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this, 'add')} />
+					<div className="todo__statics">
+						<span className="done" onClick={this.setFilter.bind(this, 'done')}>Done</span>
+						<span className="working" onClick={this.setFilter.bind(this, 'working')}>In Progress</span>
+						<span className="pending" onClick={this.setFilter.bind(this, 'pending')}>Pending</span>
+					</div>
+					{loading ? 
+					<div className="todos__loader"></div>
+					:
+					<TransitionGroup>
+					{todoList}
+					</TransitionGroup>
+					}
+				</div>
+			</div>
+		);
+	}
 }
