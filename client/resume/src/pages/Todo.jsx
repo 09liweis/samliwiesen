c0c1fa270d17614d850cc2c5f2267367340180ae
Sam Li <weisen.li@hotmail.com>
2019-09-27 22:34:45 +0800

    add todo form, edit todo

--- client/resume/src/pages/Todo.jsx
+++ client/resume/src/pages/Todo.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import {CSSTransition,TransitionGroup} from 'react-transition-group';
 
 import '../css/todo.css';
+const API = '/api/todos/';
 
 export default class Todo extends React.Component {
 	constructor(props) {
@@ -17,12 +18,17 @@ export default class Todo extends React.Component {
 				status: 'pending',
 				steps:[]
 			},
+			step:{name:'',status:'pending'},
 			filter: 'all',
 			loading: false,
-			api: 'https://samliweisen.herokuapp.com/api/todos/',
-			admin: window.localStorage.getItem('admin') || false
+			api: API,
+			admin: window.localStorage.getItem('admin') || false,
+			showForm:false
 		};
 		this.submitTodo = this.submitTodo.bind(this);
+		this.submitStep = this.submitStep.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.toggleForm = this.toggleForm.bind(this);
 	}
 	componentDidMount() {
 		this.getTodos();
@@ -42,14 +48,21 @@ export default class Todo extends React.Component {
 			this.setState({loading,error:'Network Error'});
 		});
 	}
+	submitStep() {
+		let {newTodo,step} = this.state;
+		newTodo.steps.push(step);
+		step = {name:'',status:'pending'};
+		this.setState({newTodo,step});
+	}
 	submitTodo() {
 		let {newTodo,api,todos} = this.state;
 		axios.post(api, newTodo).then((res) => {
 			if (res.status == 200) {
 				const todo = res.data;
 				todos.unshift(todo);
-				newTodo = {name:'',status:'pending'};
+				newTodo = {name:'',status:'pending',steps:[]};
 				this.setState({newTodo,todos});
+				this.toggleForm();
 			}
 		});
 	}
@@ -60,44 +73,57 @@ export default class Todo extends React.Component {
 			}
 		});
 	}
-	handleKeyPress(todo, e) {
-		if (e.key === 'Enter') {
-			if (e.target.value == 'go to admin mode') {
-				window.localStorage.setItem('admin', true);
-				this.setState({
-					admin: true,
-					newTodo: {
-						name: '',
-						status: 'pending'
-					},
-				});
-				return false;
-			}
-			if (e.target.value == 'exit admin mode') {
-				window.localStorage.setItem('admin', '');
-				this.setState({
-					admin: false,
-					newTodo: {
-						name: '',
-						status: 'pending'
-					},
-				});
-				return false;
-			}
-			if (todo != 'add') {
-				this.updateTodo(todo);
-			} else {
-				this.submitTodo();
-			}
-		}
+	// handleKeyPress(todo, e) {
+	// 	if (e.key === 'Enter') {
+	// 		if (e.target.value == 'go to admin mode') {
+	// 			window.localStorage.setItem('admin', true);
+	// 			this.setState({
+	// 				admin: true,
+	// 				newTodo: {
+	// 					name: '',
+	// 					status: 'pending'
+	// 				},
+	// 			});
+	// 			return false;
+	// 		}
+	// 		if (e.target.value == 'exit admin mode') {
+	// 			window.localStorage.setItem('admin', '');
+	// 			this.setState({
+	// 				admin: false,
+	// 				newTodo: {
+	// 					name: '',
+	// 					status: 'pending'
+	// 				},
+	// 			});
+	// 			return false;
+	// 		}
+	// 		if (todo != 'add') {
+	// 			this.updateTodo(todo);
+	// 		} else {
+	// 			this.submitTodo();
+	// 		}
+	// 	}
+	// }
+	handleEdit(idx) {
+		let {todos} = this.state;
+		const newTodo = todos[idx];
+		this.setState({newTodo});
+		this.toggleForm();
 	}
 	handleChange(e) {
-		this.setState({
-			newTodo: {
-				name: e.target.value,
-				status: 'pending'
-			}
-		});
+		const name = e.target.name;
+		const value = e.target.value;
+		if (name == 'name') {
+			this.setState({
+				newTodo: {
+					steps:[],
+					name: value,
+					status: 'pending'
+				}
+			});
+		} else {
+			this.setState({step:{name:value,status:'pending'}});
+		}
 	}
 	handleUpdate(idx, e) {
 		const newTodos = this.state.todos.map((todo, i) => {
@@ -136,9 +162,32 @@ export default class Todo extends React.Component {
 	getStatus(todo) {
 		return 'todo ' + todo.status;
 	}
+	toggleForm() {
+		const {showForm} = this.state;
+		this.setState({showForm:!showForm});
+	}
+	renderForm() {
+		const {newTodo,step} = this.state;
+		const steps = newTodo.steps.map((s,i)=>
+			<div key={i}>{s.name}</div>
+		);
+		return (
+			<div className="todo__form">
+				<input autoComplete="off" className="todo__name" placeholder="Title" name="name" value={newTodo.name} onChange={this.handleChange} />
+				<h4>Steps</h4>
+				<input autoComplete="off" className="todo__name-step" name="step" value={step.name} onChange={this.handleChange}/>
+				<span className="fa fa-plus" onClick={this.submitStep}></span>
+				{steps}
+				<a className="todo__button" onClick={this.submitTodo}>Submit</a>
+			</div>
+		);
+	}
 	render() {
-		const {admin, todos, newTodo, loading, filter,error} = this.state;
-		let todosFilter = todos,errorMsg;
+		const {admin, todos, newTodo, loading, filter,error,showForm} = this.state;
+		let todosFilter = todos,errorMsg,form;
+		if (showForm) {
+			form = this.renderForm();
+		}
 		if (error) {
 			errorMsg = <div className="todos_error">{error}</div>;
 		}
@@ -149,12 +198,13 @@ export default class Todo extends React.Component {
 			<CSSTransition key={todo._id} timeout={500} classNames="todoAnimation">
 				<div className={this.getStatus(todo)}>
 					{admin ?
-					<input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} onKeyPress={this.handleKeyPress.bind(this, todo)} />
+					<input className="todo__title" value={todo.name} onChange={this.handleUpdate.bind(this, idx)} />
 					:
 					<div className="todo__title">{todo.name}</div>
 					}
-					{todo.status != 'done' && admin ?
+					{todo.status != 'done'?
 					<div className="todo__actions">
+						<div className="todo__edit" onClick={this.handleEdit.bind(this,idx)}>Edit</div>
 						<div className="todo__complete" onClick={this.handleComplete.bind(this, idx)}>{todo.status == 'pending' ? 'Working' : 'Done'}</div>
 						<div className="todo__remove" onClick={this.handleRemove.bind(this, idx, todo._id)}>Remove</div>
 					</div>
@@ -164,9 +214,11 @@ export default class Todo extends React.Component {
 		);
 		return (
 			<div className="container">
+				{form}
 				<h2 className="todos__title">Todo List</h2>
 				<div className="todos__container">
-					<input placeholder="Add New Todo" id="todoName" value={newTodo.name} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this, 'add')} />
+					<a className="todo__button" onClick={this.toggleForm}>Add New</a>
+					{/* <input placeholder="Add New Todo" id="todoName" value={newTodo.name} onChange={this.handleChange} onKeyPress={this.handleKeyPress.bind(this, 'add')} /> */}
 					<div className="todo__statics">
 						<span className="done" onClick={this.setFilter.bind(this, 'done')}>Done</span>
 						<span className="working" onClick={this.setFilter.bind(this, 'working')}>In Progress</span>
