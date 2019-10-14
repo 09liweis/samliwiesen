ebc90772ad6d7f3c3ca095ef868834a42abfdf65
Sam Li <weisen.li@hotmail.com>
2019-10-14 15:47:07 -0400

    fix indetation for transaction model

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TransactionSchema = new Schema({
-    title: {
-        type: String,
-        required: 'Title is required for transaction'
-    },
-    price: {
-        type: Number,
-        required: 'Price is required for transaction'
-    },
-    date: {
-        type: String,
-        required: 'Date is required for transaction'
-    },
-    place: {
-        type: Schema.Types.ObjectId,
-        ref: 'Place'
-    },
-    category: String,
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    update_at: {
-        type: Date,
-        default: Date.now
-    }
+	title: {
+		type: String,
+		required: 'Title is required for transaction'
+	},
+	price: {
+		type: Number,
+		required: 'Price is required for transaction'
+	},
+	date: {
+		type: String,
+		required: 'Date is required for transaction'
+	},
+	place: {
+		type: Schema.Types.ObjectId,
+		ref: 'Place'
+	},
+	category: String,
+	created_at: {
+		type: Date,
+		default: Date.now
+	},
+	update_at: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 TransactionSchema.pre('save', (next) => {
-    const currentDate = new Date();
-    this.update_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+	const currentDate = new Date();
+	this.update_at = currentDate;
+	if (!this.created_at) {
+		this.created_at = currentDate;
+	}
+	next();
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
