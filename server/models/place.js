80f2a9be8cab451fa31c2e3eabc3dd35a68f5c4d
Sam Li <weisen.li@hotmail.com>
2019-10-15 15:39:47 -0400

    fix indetation on place model

--- server/models/place.js
+++ server/models/place.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PlaceSchema = new Schema({
-    name: String,
-    address: String,
-    lat: String,
-    lng: String,
-    place_id: String,
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    }
+	name: String,
+	address: String,
+	lat: String,
+	lng: String,
+	place_id: String,
+	created_at: {
+		type: Date,
+		default: Date.now
+	},
+	updated_at: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 PlaceSchema.pre('save', function(next) {
-    const currentDate = new Date();
-    this.updated_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+	const currentDate = new Date();
+	this.updated_at = currentDate;
+	if (!this.created_at) {
+		this.created_at = currentDate;
+	}
+	next();
 });
 
 module.exports = mongoose.model('Place', PlaceSchema);
