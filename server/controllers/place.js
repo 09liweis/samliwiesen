610552b17da927b3976fe6b80ab87c3d345c6132
Sam Li <weisen.li@hotmail.com>
2019-10-16 12:36:35 -0400

    fix indentation on place controller

--- server/controllers/place.js
+++ server/controllers/place.js
@@ -2,18 +2,18 @@ var Place = require('../models/place');
 var Transaction = require('../models/transaction');
 
 exports.place_list = (req, res) => {
-    Place.find({}, '_id name address lat lng').exec((err, places) => {
-        if (err) return res.json(err);
-        res.json(places);
-    });
+	Place.find({}, '_id name address lat lng').exec((err, places) => {
+		if (err) return res.json(err);
+		res.json(places);
+	});
 };
 
 exports.place_detail = async function(req, res) {
-    const p = await Place.findById(req.params.id);
-    const transactions = await Transaction.find({place: p._id});
-    const result = {
-        place: p,
-        transactions: transactions
-    };
-    res.json(result);
+	const p = await Place.findById(req.params.id);
+	const transactions = await Transaction.find({place: p._id});
+	const result = {
+		place: p,
+		transactions: transactions
+	};
+	res.json(result);
 };
