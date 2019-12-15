c6f0d8fc835c47c90aff8d92ca9f12a14b77948b
Sam Li <weisen.li@hotmail.com>
2019-12-15 12:44:01 -0500

    fix place detail return json

--- server/controllers/place.js
+++ server/controllers/place.js
@@ -12,7 +12,7 @@ exports.place_detail = async function(req, res) {
 	const p = await Place.findById(req.params.id);
 	const transactions = await Transaction.find({place: p._id});
 	p.transactions = transactions
-	res.json(result);
+	res.json(p);
 };
 exports.place_upsert = async function(req, res) {
 	const place = {
