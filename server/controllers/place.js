2087d3b72f15949e4c68ee2e000eed64d76c6265
Sam Li <weisen.li@hotmail.com>
2020-01-05 15:52:50 -0500

    add update random place method

--- server/controllers/place.js
+++ server/controllers/place.js
@@ -1,5 +1,34 @@
+const https = require('https');
 var Place = require('../models/place');
 var Transaction = require('../models/transaction');
+const API_KEY = 'AIzaSyA74jvNet0DufU8aoTe39dELLy2rVMeuos';
+const PLACE_DETAIL_API = 'https://maps.googleapis.com/maps/api/place/details/json?key='+API_KEY+'&place_id=';
+
+exports.place_update_random = async (req,res) =>{
+	const count = await Place.count();
+	var random = Math.floor(Math.random() * count)
+	Place.findOne().skip(random).exec((err,p)=>{
+		https.get(PLACE_DETAIL_API+p.place_id,(resp) => {
+			let data = '';
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+			resp.on('end', async () => {
+				const ret = JSON.parse(data);
+				if (ret.status == 'OK') {
+					const result = ret.result;
+					if (result.rating) {
+						p.rating = result.rating;
+					}
+				}
+				await Place.findOneAndUpdate({_id: p._id}, p, {upsert: true});
+				res.send(p);
+			});
+		}).on("error", (err) => {
+			console.log("Error: " + err.message);
+		});
+	});
+};
 
 exports.place_list = (req, res) => {
 	Place.find({}, '_id place_id name address lat lng').sort('-updated_at').exec((err, places) => {
