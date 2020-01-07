59523c7fdb589081521a342fbfdcd117a9c1f443
Sam Li <weisen.li@hotmail.com>
2020-01-06 22:54:20 -0500

    place detail get transactions with fields

--- server/controllers/place.js
+++ server/controllers/place.js
@@ -39,7 +39,7 @@ exports.place_list = (req, res) => {
 
 exports.place_detail = async function(req, res) {
 	const p = await Place.findById(req.params.id);
-	const transactions = await Transaction.find({place: p._id});
+	const transactions = await Transaction.find({place: p._id},'_id title price date category');
 	p.transactions = transactions
 	res.json(p);
 };
