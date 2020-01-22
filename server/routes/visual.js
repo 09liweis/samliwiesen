5f5c1fcd4825a4fe1010572060a9d30d79bacb42
Sam Li <weisen.li@hotmail.com>
2020-01-21 23:13:15 -0500

    fix visual headers indentation

--- server/routes/visual.js
+++ server/routes/visual.js
@@ -8,8 +8,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 const headers = {
-    'Accept-Language': 'zh-CN,zh;q=0.8',
-    'Accept-Charset': 'utf-8, iso-8859-1;q=0.5'
+	'Accept-Language': 'zh-CN,zh;q=0.8',
+	'Accept-Charset': 'utf-8, iso-8859-1;q=0.5'
 };
 const DOUBAN_SEARCH_API = 'https://movie.douban.com/j/subject_suggest?q=';
 const DOUBAN_SITE = 'https://movie.douban.com/subject/';
