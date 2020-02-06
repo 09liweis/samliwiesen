c1b1a08779f37d1cb48dce3cadde565ad3064ccb
Sam Li <weisen.li@hotmail.com>
2020-02-06 11:05:40 -0500

    fix indentation of experience.ts

--- src/app/models/experience.ts
+++ src/app/models/experience.ts
@@ -1,10 +1,10 @@
 export class Experience {
-    constructor(
-        public _id: string,
-        public title: string,
-        public company: string,
-        public start_date: string,
-        public end_date: string,
-        public duty: Array<string>,
-    ) {}
+	constructor(
+		public _id: string,
+		public title: string,
+		public company: string,
+		public start_date: string,
+		public end_date: string,
+		public duty: Array<string>,
+	) {}
 }
