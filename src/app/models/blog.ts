e4940f72d0f4b032696cf31cec0138c1b5a6d4a1
Sam Li <weisen.li@hotmail.com>
2020-02-06 11:05:12 -0500

    fix indentation of blog.ts

--- src/app/models/blog.ts
+++ src/app/models/blog.ts
@@ -1,11 +1,11 @@
 export class Blog {
-    constructor(
-        public _id: string,
-        public title: string,
-        public url: string,
-        public content: string,
-        public image: string,
-        public category: string,
-        public published: string
-    ) {}
+	constructor(
+		public _id: string,
+		public title: string,
+		public url: string,
+		public content: string,
+		public image: string,
+		public category: string,
+		public published: string
+	) {}
 }
