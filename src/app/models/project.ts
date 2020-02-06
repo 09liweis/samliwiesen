1c1e77a09a28887dca291bc40615dd97bbc2eeb7
Sam Li <weisen.li@hotmail.com>
2020-02-06 11:04:43 -0500

    fix indentation of project.ts

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,9 +1,9 @@
 export class Project {
-    constructor(
-        public _id: string,
-        public title: string,
-        public description: string,
-        public link: string,
-        public features: Array<string>,
-    ) {}
+	constructor(
+		public _id: string,
+		public title: string,
+		public description: string,
+		public link: string,
+		public features: Array<string>,
+	) {}
 }
