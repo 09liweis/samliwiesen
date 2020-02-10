bcf4e898ae95d377c3b422bff58d868cd017463c
Sam Li <weisen.li@hotmail.com>
2020-02-10 15:41:27 -0500

    indentation of webpack in resume

--- client/resume/webpack.config.js
+++ client/resume/webpack.config.js
@@ -9,18 +9,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // });
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        //can change this to dist to store in dist directory
-        path: path.resolve(''),
-        filename: '../../resume/index_bundle.js?v=' + new Date().getMilliseconds()
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-        ]
-    },
-    // plugins: [HtmlWebpackPluginConfig]
+	entry: './src/index.js',
+	output: {
+			//can change this to dist to store in dist directory
+		path: path.resolve(''),
+		filename: '../../resume/index_bundle.js?v=' + new Date().getMilliseconds()
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.css$/, loader: "style-loader!css-loader" },
+		]
+	},
+	// plugins: [HtmlWebpackPluginConfig]
 };
