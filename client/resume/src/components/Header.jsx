43ac152797559e9a4bf92f3e5ea35964b2bef6ee
Sam Li <weisen.li@hotmail.com>
2020-02-12 11:50:29 -0500

    fix indentation of header

--- client/resume/src/components/Header.jsx
+++ client/resume/src/components/Header.jsx
@@ -4,15 +4,15 @@ import Clock from './Clock.jsx';
 import Weather from './Weather.jsx';
 
 export default class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <section>
-                    What's Up! I am Sam Li
-                </section>
-                <Clock />
-                <Weather />
-            </header>
-        );
-    }
+	render() {
+		return (
+			<header id="header">
+				<section>
+					What's Up! I am Sam Li
+				</section>
+				<Clock />
+				<Weather />
+			</header>
+		);
+	}
 }
