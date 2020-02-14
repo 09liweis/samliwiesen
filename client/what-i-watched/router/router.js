764155a8e3c59c8803559924c7d251379e8d60db
Sam Li <weisen.li@hotmail.com>
2020-02-14 16:43:12 -0500

    indentation of router

--- client/what-i-watched/router/router.js
+++ client/what-i-watched/router/router.js
@@ -10,17 +10,17 @@ import ImageForm from '../views/ImageForm.vue';
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    //mode: 'history',
-    base: __dirname,
-    routes: [
-         { path: '/', component: Visuals, name: 'home' },
-         { path: '/page/:pageId', component: Visuals, name: 'page' },
-         { path: '/add', component: VisualForm },
-         { path: '/edit/:id', component: VisualForm, name: 'edit' },
-         
-         { path: '/:id/song/add', component: SongForm, name: 'addSong' },
-         { path: '/song/:songId/edit', component: SongForm, name: 'editSong' },
-         
-         { path: '/:id/image/add', component: ImageForm, name: 'addImage'}
-    ]
+	//mode: 'history',
+	base: __dirname,
+	routes: [
+		{ path: '/', component: Visuals, name: 'home' },
+		{ path: '/page/:pageId', component: Visuals, name: 'page' },
+		{ path: '/add', component: VisualForm },
+		{ path: '/edit/:id', component: VisualForm, name: 'edit' },
+		
+		{ path: '/:id/song/add', component: SongForm, name: 'addSong' },
+		{ path: '/song/:songId/edit', component: SongForm, name: 'editSong' },
+		
+		{ path: '/:id/image/add', component: ImageForm, name: 'addImage'}
+	]
 });
