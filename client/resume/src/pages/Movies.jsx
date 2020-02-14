b65369fe93fba1234beb71de8cdab8ec35f83253
Sam Li <weisen.li@hotmail.com>
2020-02-13 21:20:17 -0500

    fix indentation of movies page

--- client/resume/src/pages/Movies.jsx
+++ client/resume/src/pages/Movies.jsx
@@ -8,57 +8,53 @@ import Movie from '../components/Movie.jsx';
 import '../css/movies.css';
 
 export default class Movies extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visuals: [],
-            api: 'https://what-i-watched.herokuapp.com/api/visuals',
-            limit: 20,
-            loading: true
-        };
-        this.loadmore = this.loadmore.bind(this);
-    }
-    componentDidMount() {
-        this.setState({loading: true});
-        this.page = 1;
-        this.getVisuals();
-    }
-    loadmore() {
-        this.page += 1;
-        this.getVisuals();
-    }
-    getVisuals() {
-        const {visuals, limit, api} = this.state;
-        axios.get(api, {params: {page: this.page, limit: limit}}).then((res) => {
-            if (res.status == 200) {
-                this.setState({
-                    visuals: visuals.concat(res.data.results),
-                    loading: false
-                });
-            }
-        });
-    }
-    render() {
-        const visuals = this.state.visuals.map((v) => {
-            // status += ' visual__status';
-            return (
-                <Movie v={v} key={v.id} />
-            );
-        }
-        );
-        const loading = this.state.loading;
-        return (
-            <div className="container">
-                <h2 className="visuals__title">What I Watched</h2>
-                <div className="visual__list">
-                    {loading ?
-                        <div className="lds-hourglass"></div>
-                        :
-                        visuals   
-                    }
-                </div>
-                <div className="loadmore" onClick={this.loadmore}>Load More</div>
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			visuals: [],
+			api: 'https://what-i-watched.herokuapp.com/api/visuals',
+			limit: 20,
+			loading: true
+		};
+		this.loadmore = this.loadmore.bind(this);
+	}
+	componentDidMount() {
+		this.setState({loading: true});
+		this.page = 1;
+		this.getVisuals();
+	}
+	loadmore() {
+		this.page += 1;
+		this.getVisuals();
+	}
+	getVisuals() {
+		const {visuals, limit, api} = this.state;
+		axios.get(api, {params: {page: this.page, limit: limit}}).then((res) => {
+			if (res.status == 200) {
+				this.setState({
+					visuals: visuals.concat(res.data.results),
+					loading: false
+				});
+			}
+		});
+	}
+	render() {
+		const visuals = this.state.visuals.map((v) => {
+			// status += ' visual__status';
+			return (
+				<Movie v={v} key={v.id} />
+			);
+		}
+		);
+		const loading = this.state.loading;
+		return (
+			<div className="container">
+				<h2 className="visuals__title">What I Watched</h2>
+				<div className="visual__list">
+					{loading ?<div className="lds-hourglass"></div>:visuals}
+				</div>
+				<div className="loadmore" onClick={this.loadmore}>Load More</div>
+			</div>
+		);
+	}
 }
