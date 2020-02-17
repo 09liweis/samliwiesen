373740f5ca5bf7afcd8200f46ba57b66200faae4
Sam Li <weisen.li@hotmail.com>
2020-02-17 00:56:00 -0500

    redesign nav

--- client/resume/src/components/Nav.jsx
+++ client/resume/src/components/Nav.jsx
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const NavigationLink = styled(Link)`
-	display: inline-block;
-	margin-right: 10px;
 	color: #06A763;
 	text-decoration: none;
 	transition: 1s ease-in;
@@ -20,27 +18,38 @@ export const Navigation = styled.nav`
 	background-color: #ffffff;
 	max-width: 1200px;
 	width: 100%;
+	display:flex;
 	margin: auto;
 	box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .16), 0 2px 10px 0 rgba(0, 0, 0, .12);
 	padding: 10px;
+	justify-content:space-around;
 	@media (min-width: 768px) {
 		margin-bottom: 10px;
 		position: static;
 	}
 `;
+const navs = [
+	{url:'/',tl:'Home',icon:'fa fa-home'},
+	{url:'/movies',tl:'Movies',icon:'fa fa-film'},
+	{url:'/blogs',tl:'Blogs',icon:'fa fa-rss'},
+	{url:'/todo',tl:'Todos',icon:'fa fa-list-ol'},
+	{url:'/comments',tl:'Comments',icon:'fa fa-comments'},
+];
 
 export default class Nav extends React.Component {
 	render() {
+		const links = navs.map((nav)=>
+			<NavigationLink key={nav.url} to={nav.url}>
+				<i className={nav.icon}></i>
+				<span>{nav.tl}</span>
+			</NavigationLink>
+		);
 		return(
 			<Navigation>
-				<NavigationLink to="/">Home</NavigationLink>
-				<NavigationLink to="/movies">Movies ~</NavigationLink>
-				<NavigationLink to="/blogs">Blogs *</NavigationLink>
-				<NavigationLink to="/todo">Todos</NavigationLink>
-				<NavigationLink to="/comments">Comments</NavigationLink>
-				{ window.localStorage.getItem('admin') ?
+				{links}
+				{/* { window.localStorage.getItem('admin') ?
 				<NavigationLink to="/transactions">Transactions</NavigationLink>
-				:null}
+				:null} */}
 			</Navigation>
 		);
 	}
