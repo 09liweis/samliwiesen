9f16f972dbc737361a98296aa8a11f92acb847cb
Sam Li <weisen.li@hotmail.com>
2020-02-19 11:57:29 -0500

    add favicon

--- client/resume/src/Main.jsx
+++ client/resume/src/Main.jsx
@@ -28,7 +28,7 @@ import Comments from './pages/Comments.jsx';
 
 const store = createStore(reducer, compose(
 	applyMiddleware(thunk),
-	window.devToolsExtension ? window.devToolsExtension() : f => f
+	window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ));
 
 store.dispatch(getCharacters());
