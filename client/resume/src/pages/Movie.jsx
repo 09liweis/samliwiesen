f5e0bcd42c297a34f27c8a685b2c25fa401729aa
Sam Li <weisen.li@hotmail.com>
2020-02-24 09:14:48 -0500

    fix indentation of movie component

--- client/resume/src/pages/Movie.jsx
+++ client/resume/src/pages/Movie.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default class Movie extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <h1>Hello Movie</h1>
-        );
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<h1>Hello Movie</h1>
+		);
+	}
 }
