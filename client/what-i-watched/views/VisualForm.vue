f4a284d14a359c8a4fa633b443e2ad7952f9ffbf
Sam Li <weisen.li@hotmail.com>
2020-02-27 14:33:15 -0500

    remove multiple input, use input when update field

--- client/what-i-watched/views/VisualForm.vue
+++ client/what-i-watched/views/VisualForm.vue
@@ -1,74 +1,39 @@
 <template>
 	<div class="form">
-		<h2>Form</h2>
-		<!-- <mu-raised-button label="Add Image" class="demo-raised-button" primary v-on:click="gotoAddImage()" /> -->
-		
-		<!-- <mu-raised-button slot="actions" flat color="primary" @click="toggleSearch" v-if="visual.id == 0">Search on Douban</mu-raised-button> -->
-		<!-- <mu-dialog title="Dialog" width="360" scrollable :open.sync="searchOpen">
-			<mu-text-field fullWidth label="Search From Douban" labelFloat v-on:change="searchDouban" />
-			<mu-list>
-				<mu-list-item v-for="s in searchs" v-bind:title="s.title" :key="s.id" v-on:click="renderFromSearch(s.id)">
-				</mu-list-item>
-			</mu-list>
-			<mu-raised-button slot="actions" flat color="primary" @click="toggleSearch">Close</mu-raised-button>
-		</mu-dialog> -->
-		<mu-row gutter>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Title" labelFloat v-model="visual.title" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Original Title" labelFloat v-model="visual.original_title" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Douban Id" labelFloat v-model="visual.douban_id" v-on:change="renderDouban" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Douban Rating" labelFloat v-model="visual.douban_rating" />
-			</mu-col>
-		</mu-row>
-		<mu-raised-button label="Get Douban Data" class="demo-raised-button" v-on:click="renderDouban" secondary/>
-		<mu-row gutter>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="IMDB ID" labelFloat v-model="visual.imdb_id" v-on:change="renderIMDB" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="IMDB Rating" labelFloat v-model="visual.imdb_rating" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Episodes" labelFloat v-model="visual.episodes" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Current Episode" labelFloat v-model="visual.current_episode" />
-			</mu-col>
-		</mu-row>
-		<mu-raised-button label="Get IMDB Data" class="demo-raised-button" v-on:click="renderIMDB" secondary/>
-		<mu-row>
-			<mu-col width="100" tablet="50" desktop="25">
-					<!--<mu-text-field fullWidth label="Release Date" labelFloat v-model="visual.release_date" />-->
-				<mu-select-field label="Release Date" readonly v-model="visual.release_date" full-width>
-					<mu-menu-item v-for="release_date in release_dates" :key="release_date" :label="release_date" :value="release_date.substring(0, 10)" :title="release_date"></mu-menu-item>
-				</mu-select-field>
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Poster" labelFloat v-model="visual.poster" />
-				<img class="form-poster" v-for="p in posters" :key="p" v-bind:src="p" v-on:click="visual.poster = p" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Website" labelFloat v-model="visual.website" />
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-select-field fullWidth v-model="visual.visual_type" label="Visual Type">
-					<mu-menu-item value="movie" title="Movie"/>
-					<mu-menu-item value="tv" title="Tv" />
-				</mu-select-field>
-			</mu-col>
-			<mu-col width="100" tablet="50" desktop="25">
-				<mu-text-field fullWidth label="Duration" labelFloat v-model="visual.duration" />
-			</mu-col>
-		</mu-row>
-		<div class="form-group">
-			<mu-text-field fullWidth label="Summary" multiLine :rows="3" :rowsMax="6" labelFloat v-model="visual.summary" />
+		<div style="display:flex;">
+			<div style="width:50%;">
+				<div v-for="(v,k) in visual" :key="k">
+					<div>{{k}}: {{v}} <a @click="editField(k)">Edit</a></div>
+				</div>
+			</div>
+			<div class="input-form" style="width:50%;">
+				<label>{{currentField}}</label>
+				<input v-model="currentVal"/>
+				<a @click="updateField(currentField)">Update</a>
+			</div>
 		</div>
+		<!-- <mu-raised-button label="Add Image" class="demo-raised-button" primary v-on:click="gotoAddImage()" /> -->
+		<!-- <input label="Title" v-model="visual.title" />
+		<input label="Original Title" v-model="visual.original_title" />
+		<input label="Douban Id" v-model="visual.douban_id" v-on:change="renderDouban" />
+		<input label="Douban Rating" v-model="visual.douban_rating" />
+		<mu-raised-button label="Get Douban Data" class="demo-raised-button" v-on:click="renderDouban" secondary/>
+		<input label="IMDB ID" v-model="visual.imdb_id" v-on:change="renderIMDB" />
+		<input label="IMDB Rating" v-model="visual.imdb_rating" />
+		<input label="Episodes" v-model="visual.episodes" />
+		<input label="Current Episode" v-model="visual.current_episode" />
+		<mu-raised-button label="Get IMDB Data" class="demo-raised-button" v-on:click="renderIMDB" secondary/>
+		<mu-select-field label="Release Date" readonly v-model="visual.release_date" full-width>
+			<mu-menu-item v-for="release_date in release_dates" :key="release_date" :label="release_date" :value="release_date.substring(0, 10)" :title="release_date"></mu-menu-item>
+		</mu-select-field>
+		<input label="Poster" v-model="visual.poster" />
+		<img class="form-poster" v-for="p in posters" :key="p" v-bind:src="p" v-on:click="visual.poster = p" />
+		<input label="Website" v-model="visual.website" />
+		<input v-model="visual.visual_type" label="Visual Type"/>
+		<input label="Duration" v-model="visual.duration" />
+		<div class="form-group">
+			<input label="Summary" multiLine :rows="3" :rowsMax="6" v-model="visual.summary" />
+		</div> -->
 		<mu-raised-button label="Submit" class="demo-raised-button" primary v-on:click="handleSubmit" />
 		
 		<div class="songs">
@@ -86,6 +51,8 @@
 export default {
 	data() {
 		return {
+			currentField:'douban_id',
+			currentVal:'',
 			posters: [],
 			searchOpen: false,
 			searchs: [],
@@ -124,6 +91,13 @@ export default {
 		}
 	},
 	methods: {
+		editField(k){
+			this.currentField = k;
+			this.currentVal = this.visual[k];
+		},
+		updateField(k) {
+			this.visual[k] = this.currentVal;
+		},
 		// toggleSearch() {
 		// 	this.searchOpen = !this.searchOpen;
 		// },
@@ -264,7 +238,7 @@ export default {
 	}
 };
 </script>
-<style>
+<style scope>
 .form-poster {
   width: 50%;
 }
