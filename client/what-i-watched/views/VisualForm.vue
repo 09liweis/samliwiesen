1925b9a66962335bb7096dd03a00fcd297417252
Sam Li <weisen.li@hotmail.com>
2020-02-27 16:07:51 -0500

    render douban data

--- client/what-i-watched/views/VisualForm.vue
+++ client/what-i-watched/views/VisualForm.vue
@@ -10,40 +10,22 @@
 				<label>{{currentField}}</label>
 				<input v-model="currentVal"/>
 				<a @click="updateField(currentField)">Update</a>
+				<div v-if="currentField == 'release_date'">
+					<div v-for="d in release_dates" :key="d" @click="currentVal = d.substring(0, 10)">{{d}}</div>
+				</div>
 			</div>
 		</div>
 		<!-- <mu-raised-button label="Add Image" class="demo-raised-button" primary v-on:click="gotoAddImage()" /> -->
-		<!-- <input label="Title" v-model="visual.title" />
-		<input label="Original Title" v-model="visual.original_title" />
-		<input label="Douban Id" v-model="visual.douban_id" v-on:change="renderDouban" />
-		<input label="Douban Rating" v-model="visual.douban_rating" />
-		<mu-raised-button label="Get Douban Data" class="demo-raised-button" v-on:click="renderDouban" secondary/>
-		<input label="IMDB ID" v-model="visual.imdb_id" v-on:change="renderIMDB" />
-		<input label="IMDB Rating" v-model="visual.imdb_rating" />
-		<input label="Episodes" v-model="visual.episodes" />
-		<input label="Current Episode" v-model="visual.current_episode" />
-		<mu-raised-button label="Get IMDB Data" class="demo-raised-button" v-on:click="renderIMDB" secondary/>
-		<mu-select-field label="Release Date" readonly v-model="visual.release_date" full-width>
-			<mu-menu-item v-for="release_date in release_dates" :key="release_date" :label="release_date" :value="release_date.substring(0, 10)" :title="release_date"></mu-menu-item>
-		</mu-select-field>
-		<input label="Poster" v-model="visual.poster" />
-		<img class="form-poster" v-for="p in posters" :key="p" v-bind:src="p" v-on:click="visual.poster = p" />
-		<input label="Website" v-model="visual.website" />
-		<input v-model="visual.visual_type" label="Visual Type"/>
-		<input label="Duration" v-model="visual.duration" />
-		<div class="form-group">
-			<input label="Summary" multiLine :rows="3" :rowsMax="6" v-model="visual.summary" />
-		</div> -->
-		<mu-raised-button label="Submit" class="demo-raised-button" primary v-on:click="handleSubmit" />
+		<button v-on:click="handleSubmit">Submit</button>
 		
-		<div class="songs">
+		<!-- <div class="songs">
 			<mu-raised-button label="Add Song" class="demo-raised-button" primary v-on:click="gotoAddSong()" />
 			<mu-list>
 				<mu-list-item v-for="s in songs" v-bind:title="s.title" :key="s.id" v-on:click="editSong(s)">
 					<mu-avatar v-bind:src="s.image" slot="rightAvatar"/>
 				</mu-list-item>
 			</mu-list>
-		</div>
+		</div> -->
 	</div>
 </template>
 
@@ -97,6 +79,9 @@ export default {
 		},
 		updateField(k) {
 			this.visual[k] = this.currentVal;
+			if (k == 'douban_id') {
+				this.renderDouban();
+			}
 		},
 		// toggleSearch() {
 		// 	this.searchOpen = !this.searchOpen;
@@ -202,7 +187,7 @@ export default {
 		},
 		renderIMDB() {
 			if (this.visual.imdb_id == '') return;
-				const options = {
+			const options = {
 				params: {
 					i: this.visual.imdb_id,
 					apikey: '6ad10fa5'
