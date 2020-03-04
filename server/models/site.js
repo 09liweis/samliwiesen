1c5bfee101b6be2afd5606c732db20c3cdfdc668
Sam Li <weisen.li@hotmail.com>
2020-03-04 08:59:55 -0500

    fix indentation of site model

--- server/models/site.js
+++ server/models/site.js
@@ -2,31 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SiteSchema = new Schema({
-    name: {
-        type: String,
-        default: 'Samliweisen'
-    },
-    visit: {
-        type: Number,
-        default: 0
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    update_at: {
-        type: Date,
-        default: Date.now
-    },
+	name: {
+		type: String,
+		default: 'Samliweisen'
+	},
+	visit: {
+		type: Number,
+		default: 0
+	},
+	created_at: {
+		type: Date,
+		default: Date.now
+	},
+	update_at: {
+		type: Date,
+		default: Date.now
+	},
 });
 
 SiteSchema.pre('save', (next) => {
-    const currentDate = new Date();
-    this.update_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+	const currentDate = new Date();
+	this.update_at = currentDate;
+	if (!this.created_at) {
+		this.created_at = currentDate;
+	}
+	next();
 });
 
 module.exports = mongoose.model('Site', SiteSchema);
