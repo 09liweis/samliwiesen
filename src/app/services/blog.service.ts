993dd43d5976d74fa9406cb79a3a770f2b6afd21
Sam Li <weisen.li@hotmail.com>
2020-03-13 23:07:57 -0400

    update rxjs

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs';
 
 import { Blog } from '../models/blog';
 import {genAPI} from '../constant';
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable} from 'rxjs/Rx';
+import {Observable} from 'rxjs';
 
 import { Experience } from '../models/experience';
 import {genAPI} from '../constant';
