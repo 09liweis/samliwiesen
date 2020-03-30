200dcf4789d738b86541c8e76181c73768359796
Sam Li <weisen.li@hotmail.com>
2020-03-29 20:27:33 -0400

    add nav to reducer index

--- client/resume/src/reducers/index.js
+++ client/resume/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import characters from './characters';
 import projects from './projects';
 import experiences from './experiences.js';
+import nav from './nav.js';
 import skills from './skills.js';
 import todos from './todos.js';
 
 export default combineReducers({
+	nav,
 	projects,
 	experiences,
 	skills,
