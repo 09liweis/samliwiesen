9e24c6cf03d4d243d68f2266e666fd76d1d783f6
Sam Li <weisen.li@hotmail.com>
2020-03-29 22:32:03 -0400

    setnav action

--- client/resume/src/actions/nav.js
+++ client/resume/src/actions/nav.js
@@ -1,5 +1,14 @@
-export const setNav = () => {
+export const setNav = (navId) => {
+  console.log(navId);
+  const el = document.getElementById(navId);
+  const elData = el.getBoundingClientRect();
+  let offset = 0;
+  if (window.innerWidth >= 1200) {
+    offset = (window.innerWidth - 1200)/2;
+  }
+  console.log({highLightPosLeft:elData.left-offset,highLightPosWidth:elData.width});
   return {
-    type:'SET_NAV'
+    type:'SET_NAV',
+    payload:{highLightPosId:navId,highLightPosLeft:elData.left-offset,highLightPosWidth:elData.width}
   };
 };
