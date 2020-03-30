b0f6c4f9a94a25fcad8809f7ea8bbeacaf24ac3e
Sam Li <weisen.li@hotmail.com>
2020-03-30 00:24:30 -0400

    update nav reducer

--- client/resume/src/reducers/nav.js
+++ client/resume/src/reducers/nav.js
@@ -1,7 +1,11 @@
-const navReducer = (state = {highLightPosId:'home',highLightPosLeft:0,highLightPosWidth:0}, action) => {
+const navReducer = (state = {highLightPosId:'',highLightPosLeft:0,highLightPosWidth:0}, action) => {
 	switch(action.type) {
 		case 'SET_NAV':
 			return action.payload;
+		case 'HOVER_NAV':
+			const highLightPosLeft = action.payload.highLightPosLeft;
+			const highLightPosWidth = action.payload.highLightPosWidth;
+			return {highLightPosId:state.highLightPosId,highLightPosLeft,highLightPosWidth}
 		default:
 			return state;
 	}
