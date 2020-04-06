6a6bb5019734203981af351d9b7e0ec1eafe873f
Sam Li <weisen.li@hotmail.com>
2020-04-06 18:28:50 -0400

    convert blogs compoent to redux

--- client/resume/src/pages/Blogs.jsx
+++ client/resume/src/pages/Blogs.jsx
@@ -1,55 +1,41 @@
 import React from 'react';
-import axios from 'axios';
+import { useSelector,useDispatch } from 'react-redux';
 import {Box, BoxTitle, BoxBody} from '../components/style.jsx';
+import {getBlogs} from '../actions/blog';
 
 import '../css/blogs.css';
 
-export default class Blogs extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			blogs: [],
-			api: 'https://samliweisen.herokuapp.com/api/blogs'
-		};
+const Blogs = () => {
+	const dispatch = useDispatch();
+	const blogs = useSelector(state => state.blogs);
+	if (blogs && blogs.length === 0) {
+		dispatch(getBlogs());
 	}
-	componentDidMount() {
-		this.getBlogs();
-	}
-	getBlogs() {
-		axios.get(this.state.api).then((res) => {
-			if (res.status == 200) {
-				this.setState({
-					blogs: res.data
-				});
-			}
-		});
-	}
-	render() {
-		const blogs = this.state.blogs.map((b) => {
-			return (
-				<article className="blog" key={b._id}>
-					<h3 className="blog__title">{b.title}</h3>
-					<div className="blog__attr">
-						<span className="blog__date">{b.created_at.substr(0, 10) }</span>
-						<span className="blog__category">{b.category}</span>
-					</div>
-					{b.image ?
-					<img className="blog__image" src={b.image} />
-					: null}
-					<section dangerouslySetInnerHTML={{ __html:'' }}></section>
-				</article>
-			);
-		});
+	const lst = blogs.map((b) => {
 		return (
-			<Box className="movies">
-				<BoxTitle>
-					<i className="boxIcon fa fa-film" aria-hidden="true"></i>
-					<span>Blogs</span>
-				</BoxTitle>
-				<BoxBody>
-					{blogs}
-				</BoxBody>
-			</Box>
+			<article className="blog" key={b._id}>
+				<h3 className="blog__title">{b.title}</h3>
+				<div className="blog__attr">
+					<span className="blog__date">{b.created_at.substr(0, 10) }</span>
+					<span className="blog__category">{b.category}</span>
+				</div>
+				{b.image ?
+				<img className="blog__image" src={b.image} />
+				: null}
+				<section dangerouslySetInnerHTML={{ __html:'' }}></section>
+			</article>
 		);
-	}
+	});
+	return (
+		<Box className="movies">
+			<BoxTitle>
+				<i className="boxIcon fa fa-film" aria-hidden="true"></i>
+				<span>Blogs</span>
+			</BoxTitle>
+			<BoxBody>
+				{lst}
+			</BoxBody>
+		</Box>
+	);
 }
+export default Blogs;
