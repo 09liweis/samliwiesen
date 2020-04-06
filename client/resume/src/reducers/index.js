dccea36b612b70454a48e713f55e82aa2e2f489f
Sam Li <weisen.li@hotmail.com>
2020-04-06 17:02:24 -0400

    import blog reducer to index reducer

--- client/resume/src/reducers/index.js
+++ client/resume/src/reducers/index.js
@@ -3,10 +3,12 @@ import characters from './characters';
 import projects from './projects';
 import experiences from './experiences.js';
 import nav from './nav.js';
+import blogs from './blogs.js';
 import skills from './skills.js';
 import todos from './todos.js';
 
 export default combineReducers({
+	blogs,
 	nav,
 	projects,
 	experiences,
