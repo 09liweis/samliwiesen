0005bca43e4a952d731c376b83e194eafe784d31
Sam Li <weisen.li@hotmail.com>
2020-04-06 17:01:59 -0400

    add blog actions

---
+++ client/resume/src/actions/blog.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getBlogs = () => {
+  return dispatch=> {
+    axios.get('https://samliweisen.herokuapp.com/api/blogs?origin=localhost')
+    .then(ex=>
+      dispatch({type:'GET_BLOGS',ex:ex.data})
+    );
+  }
+};
