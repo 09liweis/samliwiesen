fbaa707f7e9ca38f23a646e39fc51b57818c2e30
Sam Li <weisen.li@hotmail.com>
2020-04-06 18:28:28 -0400

    update blog action dispatch object key

--- client/resume/src/actions/blog.js
+++ client/resume/src/actions/blog.js
@@ -4,7 +4,7 @@ export const getBlogs = () => {
   return dispatch=> {
     axios.get('https://samliweisen.herokuapp.com/api/blogs?origin=localhost')
     .then(ex=>
-      dispatch({type:'GET_BLOGS',ex:ex.data})
+      dispatch({type:'GET_BLOGS',blogs:ex.data})
     );
   }
 };
