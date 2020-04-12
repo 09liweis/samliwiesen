2547ca8b55496ec984d58bb8996a90d67e5d16ab
Sam Li <weisen.li@hotmail.com>
2020-04-11 21:32:02 -0400

    update tab to space

--- client/resume/src/pages/Blogs.jsx
+++ client/resume/src/pages/Blogs.jsx
@@ -6,36 +6,36 @@ import {getBlogs} from '../actions/blog';
 import '../css/blogs.css';
 
 const Blogs = () => {
-	const dispatch = useDispatch();
-	const blogs = useSelector(state => state.blogs);
-	if (blogs && blogs.length === 0) {
-		dispatch(getBlogs());
-	}
-	const lst = blogs.map((b) => {
-		return (
-			<article className="blog" key={b._id}>
-				<h3 className="blog__title">{b.title}</h3>
-				<div className="blog__attr">
-					<span className="blog__date">{b.created_at.substr(0, 10) }</span>
-					<span className="blog__category">{b.category}</span>
-				</div>
-				{b.image ?
-				<img className="blog__image" src={b.image} />
-				: null}
-				<section dangerouslySetInnerHTML={{ __html:'' }}></section>
-			</article>
-		);
-	});
-	return (
-		<Box className="movies">
-			<BoxTitle>
-				<i className="boxIcon fa fa-film" aria-hidden="true"></i>
-				<span>Blogs</span>
-			</BoxTitle>
-			<BoxBody>
-				{lst}
-			</BoxBody>
-		</Box>
-	);
+  const dispatch = useDispatch();
+  const blogs = useSelector(state => state.blogs);
+  if (blogs && blogs.length === 0) {
+    dispatch(getBlogs());
+  }
+  const lst = blogs.map((b) => {
+    return (
+      <article className="blog" key={b._id}>
+        <h3 className="blog__title">{b.title}</h3>
+        <div className="blog__attr">
+          <span className="blog__date">{b.created_at.substr(0, 10) }</span>
+          <span className="blog__category">{b.category}</span>
+        </div>
+        {b.image ?
+        <img className="blog__image" src={b.image} />
+        : null}
+        <section dangerouslySetInnerHTML={{ __html:'' }}></section>
+      </article>
+    );
+  });
+  return (
+    <Box className="movies">
+      <BoxTitle>
+        <i className="boxIcon fa fa-film" aria-hidden="true"></i>
+        <span>Blogs</span>
+      </BoxTitle>
+      <BoxBody>
+        {lst}
+      </BoxBody>
+    </Box>
+  );
 }
 export default Blogs;
