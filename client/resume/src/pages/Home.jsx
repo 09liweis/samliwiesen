ddf565e6ce5f7b79b3d8a4cb53e56c06acc27bb0
Sam Li <weisen.li@hotmail.com>
2020-04-11 21:33:42 -0400

    fix home tab to space

--- client/resume/src/pages/Home.jsx
+++ client/resume/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect,useSelector,useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Experiences from '../components/home/Experiences.jsx';
 import Skills from '../components/home/Skills.jsx';
@@ -8,18 +8,13 @@ import Projects from '../components/home/Projects.jsx';
 import '../css/resume.css';
 // const Home  = ({characters}) => 
 const Home = () => {
-	const dispatch = useDispatch();
-	return (
-		<div className="home">
-			<Experiences />
-			<Projects />
-			<Skills />
-		</div>
-	);
+  const dispatch = useDispatch();
+  return (
+    <div className="home">
+      <Experiences />
+      <Projects />
+      <Skills />
+    </div>
+  );
 };
 export default Home;
-// const mapStateToProps = ({characters}) => ({
-//   characters
-// });
-
-// export default connect(mapStateToProps)(Home);
