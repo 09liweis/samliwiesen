3d345c646402c4e13a16b542b404c161f828b4af
Sam Li <weisen.li@hotmail.com>
2020-04-16 16:34:29 -0400

    fix tab to space index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/').get(function(req, res) {
-    res.status(200).json('Resume site comming soon');
+  res.status(200).json('Resume site comming soon');
 });
-
-// router.route('/dashboard').get(function(req, res) {
-//     res.status(200).json('Resume site comming soon');
-// });
 
 module.exports = router;
