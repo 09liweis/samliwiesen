889eca7f1663063b018aa256ebf49bf0a4ebc355
Sam Li <weisen.li@hotmail.com>
2020-04-16 16:33:53 -0400

    fix tab to space dashboard route

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,13 +5,13 @@ const Experience = require('../models/experience');
 const Project = require('../models/project');
 
 router.route('/').get(function(req, res) {
-	Blog.count().exec((err, blogs) => {
-		Experience.count().exec((err,exs)=>{
-			Project.count().exec((err,projs)=>{
-				res.status(200).json({blogs,exs,projs});
-			});
-		})
-	});
+  Blog.count().exec((err, blogs) => {
+    Experience.count().exec((err,exs)=>{
+      Project.count().exec((err,projs)=>{
+        res.status(200).json({blogs,exs,projs});
+      });
+    })
+  });
 });
 
 module.exports = router;
