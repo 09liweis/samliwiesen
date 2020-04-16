7ec6d2a9c733072c0075623fa412061406d73f37
Sam Li <weisen.li@hotmail.com>
2020-04-15 22:10:54 -0400

    fix tab to space in user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,27 +1,27 @@
 const User = require('../models/user');
 
 exports.list = (req, res) => {
-	User.find({}, '_id title url content image category published created_at').sort('-created_at').exec((err, users) => {
-		handleError(res, err);
-		res.json(users);
-	});
+  User.find({}, '_id title url content image category published created_at').sort('-created_at').exec((err, users) => {
+    handleError(res, err);
+    res.json(users);
+  });
 };
 exports.add = async (req,res)=>{
-	const body = req.body;
-	const eml = body.eml;
-	let user = await User.findOne({eml})
-	let msg = 'ok';
-	if (user) {
-		msg = 'Email has been registered';
-		return res.status(200).json({msg});
-	} else {
-		user = new User(body);
-		// await user.save()
-		res.status(200).json({msg});
-	}
+  const body = req.body;
+  const eml = body.eml;
+  let user = await User.findOne({eml})
+  let msg = 'ok';
+  if (user) {
+    msg = 'Email has been registered';
+    return res.status(200).json({msg});
+  } else {
+    user = new User(body);
+    // await user.save()
+    res.status(200).json({msg});
+  }
 }
 function handleError(res, err) {
-	if (err) {
-		res.send(err);
-	}
+  if (err) {
+    res.send(err);
+  }
 }
