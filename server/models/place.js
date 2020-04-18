c87b9f58509d141c5267d574e27fd5f1cec0f075
Sam Li <weisen.li@hotmail.com>
2020-04-18 11:35:58 -0400

    tab to space place model

--- server/models/place.js
+++ server/models/place.js
@@ -2,33 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PlaceSchema = new Schema({
-	name: String,
-	address: String,
-	lat: String,
-	lng: String,
-	place_id: String,
-	rating:Number,
-	icon:String,
-	created_at: {
-		type: Date,
-		default: Date.now
-	},
-	updated_at: {
-		type: Date,
-		default: Date.now
-	},
-	photos:Array,
-	types:Array,
-	transactions:Array
+  name: String,
+  address: String,
+  lat: String,
+  lng: String,
+  place_id: String,
+  rating:Number,
+  icon:String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  },
+  photos:Array,
+  types:Array,
+  transactions:Array
 });
 
 PlaceSchema.pre('save', function(next) {
-	const currentDate = new Date();
-	this.updated_at = currentDate;
-	if (!this.created_at) {
-		this.created_at = currentDate;
-	}
-	next();
+  const currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Place', PlaceSchema);
