693539cde19e838f652fb064e464abf059be7138
Sam Li <weisen.li@hotmail.com>
2020-04-18 11:15:25 -0400

    reduce space experience model

--- server/models/experience.js
+++ server/models/experience.js
@@ -2,30 +2,30 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ExperienceSchema = new Schema({
-    title: String,
-    company: String,
-    start_date: String,
-    end_date: String,
-    duty: [{
-        type: String
-    }],
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    }
+  title: String,
+  company: String,
+  start_date: String,
+  end_date: String,
+  duty: [{
+    type: String
+  }],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 ExperienceSchema.pre('save', (next) => {
-    const currentDate = new Date();
-    this.updated_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+  const currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Experience', ExperienceSchema);
