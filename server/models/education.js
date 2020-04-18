aacf4cafef19f02e0777c32728653510ac80e08e
Sam Li <weisen.li@hotmail.com>
2020-04-18 11:12:05 -0400

    reduce space on education model

--- server/models/education.js
+++ server/models/education.js
@@ -2,31 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EducationSchema = new Schema({
-    school: {
-        type: Schema.Types.ObjectId,
-        ref: 'Place'
-    },
-    degree: String,
-    subject: String,
-    from: String,
-    to: String,
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    update_at: {
-        type: Date,
-        default: Date.now
-    }
+  school: {
+    type: Schema.Types.ObjectId,
+    ref: 'Place'
+  },
+  degree: String,
+  subject: String,
+  from: String,
+  to: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 EducationSchema.pre('save', function(next) {
-    const currentDate = new Date();
-    this.update_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+  const currentDate = new Date();
+  this.update_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Education', EducationSchema);
