df60a8ff7c031f51cb83b969146bb73d8a400ecb
Sam Li <weisen.li@hotmail.com>
2020-04-18 10:55:27 -0400

    fix tab to space on project controller

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -2,46 +2,46 @@ var mongoose = require('mongoose'),
 Project = require('../models/project');
 
 exports.project_list = (req, res) => {
-	Project.find({}).sort('-created_at').exec((err, projects) => {
-		handleError(res, err);
-		res.json(projects);
-	});
+  Project.find({}).sort('-created_at').exec((err, projects) => {
+    handleError(res, err);
+    res.json(projects);
+  });
 };
 
 exports.project_new = (req, res) => {
-	const newProject = new Project(req.body);
-	newProject.save((err, project) => {
-		handleError(res, err);
-		res.json(project);
-	});
+  const newProject = new Project(req.body);
+  newProject.save((err, project) => {
+    handleError(res, err);
+    res.json(project);
+  });
 };
 
 exports.project_detail = (req, res) => {
-	Project.findById(req.params.id, (err, project) => {
-		handleError(res, err);
-		res.json(project);
-	});
+  Project.findById(req.params.id, (err, project) => {
+    handleError(res, err);
+    res.json(project);
+  });
 };
 
 exports.project_update = (req, res) => {
-	let updateProject = req.body;
-	updateProject.update_at = new Date();
-	Project.findOneAndUpdate({_id: req.params.id}, updateProject, {upsert: true}, (err, project) => {
-		handleError(res, err);
-		res.json(project);
-	});
+  let updateProject = req.body;
+  updateProject.update_at = new Date();
+  Project.findOneAndUpdate({_id: req.params.id}, updateProject, {upsert: true}, (err, project) => {
+    handleError(res, err);
+    res.json(project);
+  });
 };
 
 exports.project_delete = (req, res) =>{
-	Project.remove({_id: req.params.id}, (err) =>{
-		handleError(res, err);
-		res.json({ok:1,msg:'Project Deleted'});
-	});
+  Project.remove({_id: req.params.id}, (err) =>{
+    handleError(res, err);
+    res.json({ok:1,msg:'Project Deleted'});
+  });
 };
 
 
 function handleError(res, err) {
-	if (err) {
-		res.send(err);
-	}
+  if (err) {
+    res.send(err);
+  }
 }
