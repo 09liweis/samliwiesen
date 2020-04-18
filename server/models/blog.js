35e9fee3a0e9a3cf3eabbabe3730bdf6bae6f828
Sam Li <weisen.li@hotmail.com>
2020-04-18 11:04:41 -0400

    tab to space on blog model

--- server/models/blog.js
+++ server/models/blog.js
@@ -2,32 +2,32 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const BlogSchema = new Schema({
-	title: String,
-	url: String,
-	content: String,
-	image: String,
-	category: String,
-	published: {
-		type: String,
-		default: '1' // 1: published, 0: draft
-	},
-	created_at: {
-		type: Date,
-		default: Date.now
-	},
-	update_at: {
-		type: Date,
-		default: Date.now
-	}
+  title: String,
+  url: String,
+  content: String,
+  image: String,
+  category: String,
+  published: {
+    type: String,
+    default: '1' // 1: published, 0: draft
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 BlogSchema.pre('save', function(next) {
-	const currentDate = new Date();
-	this.update_at = currentDate;
-	if (!this.created_at) {
-		this.created_at = currentDate;
-	}
-	next();
+  const currentDate = new Date();
+  this.update_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
