611c3cfdfb1f626bc6b5e5b86ffb1c2a651a7c05
Sam Li <weisen.li@hotmail.com>
2020-04-18 21:21:00 -0400

    tab to space project model

--- server/models/project.js
+++ server/models/project.js
@@ -2,36 +2,36 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-    title: {
-        type: String,
-        required: 'Kindly enter the name of the task'
-    },
-    description: {
-        type: String
-    },
-    features: [{
-        type: String
-    }],
-    link: {
-        type: String
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    }
+  title: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  description: {
+    type: String
+  },
+  features: [{
+    type: String
+  }],
+  link: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 ProjectSchema.pre('save', (next) => {
-    const currentDate = new Date();
-    this.updated_at = currentDate;
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
+  const currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
