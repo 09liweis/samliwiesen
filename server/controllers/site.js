a13a5fa38a0ce1942979ec9069f5bb5ee4ba2538
Sam Li <weisen.li@hotmail.com>
2020-04-19 23:10:31 -0400

    tab to space site controller

--- server/controllers/site.js
+++ server/controllers/site.js
@@ -1,23 +1,23 @@
 var Site = require('../models/site');
 
 exports.site_detail = (req, res) => {
-	Site.findById(req.params.id, (err, site) => {
-		handleError(res, err);
-		res.json(site);
-	});
+  Site.findById(req.params.id, (err, site) => {
+    handleError(res, err);
+    res.json(site);
+  });
 };
 
 exports.site_update = (req, res) => {
-	let updateSite = req.body;
-	updateSite.update_at = new Date();
-	Site.findOneAndUpdate({_id: req.params.id}, updateSite, {upsert: true}, (err, site) => {
-		handleError(res, err);
-		res.json(site);
-	});
+  let updateSite = req.body;
+  updateSite.update_at = new Date();
+  Site.findOneAndUpdate({_id: req.params.id}, updateSite, {upsert: true}, (err, site) => {
+    handleError(res, err);
+    res.json(site);
+  });
 };
 
 function handleError(res, err) {
-	if (err) {
-		res.send(err);
-	}
+  if (err) {
+    res.send(err);
+  }
 }
