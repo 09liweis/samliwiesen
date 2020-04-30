e1aaac36372b191e4aee0488f8a4356c1653326d
Sam Li <weisen.li@hotmail.com>
2020-04-30 14:15:40 -0400

    tab to space blog controller

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,45 +1,45 @@
 const Blog = require('../models/blog');
 
 exports.blog_list = (req, res) => {
-	Blog.find({}, '_id title url content image category published created_at').sort('-created_at').exec((err, blogs) => {
-		handleError(res, err);
-		res.json(blogs);
-	});
+  Blog.find({}, '_id title url content image category published created_at').sort('-created_at').exec((err, blogs) => {
+    handleError(res, err);
+    res.json(blogs);
+  });
 };
 
 exports.blog_add = (req, res) => {
-	const newBlog = new Blog(req.body);
-	newBlog.save((err, blog) => {
-		handleError(res, err);
-		res.json(blog);
-	});
+  const newBlog = new Blog(req.body);
+  newBlog.save((err, blog) => {
+    handleError(res, err);
+    res.json(blog);
+  });
 };
 
 exports.blog_detail = (req, res) => {
-	Blog.findById(req.params.id, (err, blog) => {
-		handleError(res, err);
-		res.json(blog);
-	});
+  Blog.findById(req.params.id, (err, blog) => {
+    handleError(res, err);
+    res.json(blog);
+  });
 };
 
 exports.blog_update = (req, res) => {
-	let updateblog = req.body;
-	updateblog.update_at = new Date();
-	Blog.findOneAndUpdate({_id: req.params.id}, updateblog, {upsert: true}, (err, blog) => {
-		handleError(res, err);
-		res.json(blog);
-	});
+  let updateblog = req.body;
+  updateblog.update_at = new Date();
+  Blog.findOneAndUpdate({_id: req.params.id}, updateblog, {upsert: true}, (err, blog) => {
+    handleError(res, err);
+    res.json(blog);
+  });
 };
 
 exports.blog_delete = (req, res) => {
-	Blog.remove({_id: req.params.id}, (err) => {
-		handleError(res, err);
-		res.status(200).json('ok');
-	});
+  Blog.remove({_id: req.params.id}, (err) => {
+    handleError(res, err);
+    res.status(200).json('ok');
+  });
 };
 
 function handleError(res, err) {
-	if (err) {
-		res.send(err);
-	}
+  if (err) {
+    res.send(err);
+  }
 }
