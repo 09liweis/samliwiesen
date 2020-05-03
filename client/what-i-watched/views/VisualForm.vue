098f5552065c82615bdec746835a465f1089074e
Sam Li <weisen.li@hotmail.com>
2020-05-02 23:48:14 -0400

    tab to space visualform.vue

--- client/what-i-watched/views/VisualForm.vue
+++ client/what-i-watched/views/VisualForm.vue
@@ -1,239 +1,239 @@
 <template>
-	<div class="form">
-		<div style="display:flex;">
-			<div style="width:50%;">
-				<div v-for="(v,k) in visual" :key="k">
-					<div>
-						<span>{{k}}:</span>
-						<img v-if="k=='poster'" :src="v"/>
-						<span v-else>{{v}}</span>
-						<a @click="editField(k)">Edit</a></div>
-				</div>
-			</div>
-			<div class="input-form" style="width:50%;">
-				<label>{{currentField}}</label>
-				<input v-model="currentVal"/>
-				<a @click="updateField(currentField)">Update</a>
-				<div v-if="currentField == 'release_date'">
-					<div v-for="d in release_dates" :key="d" @click="currentVal = d.substring(0, 10)">{{d}}</div>
-				</div>
-				<div style="display:flex;" v-if="currentField == 'poster' && posters.length > 0">
-					<img style="width:33%;" v-for="p in posters" :key="p" :src="p" @click="currentVal=p"/>
-				</div>
-			</div>
-		</div>
-		<!-- <mu-raised-button label="Add Image" class="demo-raised-button" primary v-on:click="gotoAddImage()" /> -->
-		<button v-on:click="handleSubmit">Submit</button>
-		
-		<!-- <div class="songs">
-			<mu-raised-button label="Add Song" class="demo-raised-button" primary v-on:click="gotoAddSong()" />
-			<mu-list>
-				<mu-list-item v-for="s in songs" v-bind:title="s.title" :key="s.id" v-on:click="editSong(s)">
-					<mu-avatar v-bind:src="s.image" slot="rightAvatar"/>
-				</mu-list-item>
-			</mu-list>
-		</div> -->
-	</div>
+  <div class="form">
+    <div style="display:flex;">
+      <div style="width:50%;">
+        <div v-for="(v,k) in visual" :key="k">
+          <div>
+            <span>{{k}}:</span>
+            <img v-if="k=='poster'" :src="v"/>
+            <span v-else>{{v}}</span>
+            <a @click="editField(k)">Edit</a></div>
+        </div>
+      </div>
+      <div class="input-form" style="width:50%;">
+        <label>{{currentField}}</label>
+        <input v-model="currentVal"/>
+        <a @click="updateField(currentField)">Update</a>
+        <div v-if="currentField == 'release_date'">
+          <div v-for="d in release_dates" :key="d" @click="currentVal = d.substring(0, 10)">{{d}}</div>
+        </div>
+        <div style="display:flex;" v-if="currentField == 'poster' && posters.length > 0">
+          <img style="width:33%;" v-for="p in posters" :key="p" :src="p" @click="currentVal=p"/>
+        </div>
+      </div>
+    </div>
+    <!-- <mu-raised-button label="Add Image" class="demo-raised-button" primary v-on:click="gotoAddImage()" /> -->
+    <button v-on:click="handleSubmit">Submit</button>
+    
+    <!-- <div class="songs">
+      <mu-raised-button label="Add Song" class="demo-raised-button" primary v-on:click="gotoAddSong()" />
+      <mu-list>
+        <mu-list-item v-for="s in songs" v-bind:title="s.title" :key="s.id" v-on:click="editSong(s)">
+          <mu-avatar v-bind:src="s.image" slot="rightAvatar"/>
+        </mu-list-item>
+      </mu-list>
+    </div> -->
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			currentField:'douban_id',
-			currentVal:'',
-			posters: [],
-			searchOpen: false,
-			searchs: [],
-			visual: {
-				id: 0,
-				title: '',
-				original_title: '',
-				douban_id: '',
-				douban_rating: '0.0',
-				imdb_id: '',
-				imdb_rating: '0.0',
-				rotten_id: '',
-				rotten_rating: 0,
-				rotten_audience_rating: 0,
-				release_date: '',
-				poster: '',
-				summary: '',
-				online_source: '',
-				episodes: 1,
-				current_episode: 0,
-				visual_type: 'movie',
-				website: '',
-				languages:[],
-				countries:[],
-				duration:0,
-			},
-			release_dates: [],
-			songs: []
-		};
-	},
-	mounted() {
-		const id = this.$route.params.id;
-		if (typeof id != 'undefined') {
-			this.getVisual(id);
-			this.getSongs(id);
-		}
-	},
-	methods: {
-		editField(k){
-			this.currentField = k;
-			this.currentVal = this.visual[k];
-		},
-		updateField(k) {
-			this.visual[k] = this.currentVal;
-			if (k == 'douban_id') {
-				this.renderDouban();
-			}
-		},
-		// toggleSearch() {
-		// 	this.searchOpen = !this.searchOpen;
-		// },
-		gotoAddSong() {
-			this.$router.push({path: '/' + this.$route.params.id + '/song/add'});
-		},
-		// gotoAddImage() {
-		// 	this.$router.push({path: '/' + this.$route.params.id + '/image/add'});
-		// },
-		renderFromSearch(id) {
-			this.toggleSearch();
-			this.visual.douban_id = id;
-			this.renderDouban();
-		},
-		handleSubmit() {
-			const options = this.visual;
-			this.$http.post(this.$store.state.api.visualSubmit, options).then(res => {
-				if (res.status == 200) {
-					if (res.body.code == 'exist') {
-						alert('Douban Id exist');
-					} else {
-						this.$router.push({ path: '/' });
-					}
-				}
-			});
-		},
-		getVisual(id) {
-			this.$http.get(this.$store.state.api.visualDetail + id).then(res => {
-				this.visual = res.body.result;
-				if (this.visual.douban_id) {
-					this.renderDouban();
-				}
-			});
-		},
-		// searchDouban(e) {
-		// 	const val = e.target.value;
-		// 	const api = '/api/visuals/search?keyword=' + val;
-		// 	// 'https://api.douban.com/v2/movie/search?q=' + val + '&apikey=0df993c66c0c636e29ecbb5344252a4a'
-		// 	this.$http.get(api).then(res => {
-		// 		this.searchs = [];
-		// 		if (res.status == 200) {
-		// 			console.log(res.body);
-		// 			this.searchs = res.body;
-		// 		}
-		// 	});
-		// },
-		renderDouban() {
-			if (this.visual.douban_id == '') {
-				alert('Empty douban id');
-				return;
-			}
-			this.getImdbId();
-			this.$http.jsonp('https://api.douban.com/v2/movie/subject/' + this.visual.douban_id + '?apikey=0df993c66c0c636e29ecbb5344252a4a').then(res => {
-				const douban = res.body;
-				if (this.visual.summary == '') {
-					this.visual.summary = douban.summary;
-				}
-				this.release_dates = douban.pubdates;
-				this.posters = [];
-				if (this.visual.poster) {
-					this.posters.push(this.visual.poster);
-				}
-				this.posters.push(douban.images.large);
-				
-				this.visual.title = douban.title;
-				if (!this.visual.original_title) {
-					this.visual.original_title = douban.original_title;
-				}
-				this.visual.douban_rating = douban.rating.average;
-				if (douban.episodes_count) {
-					this.visual.episodes = douban.episodes_count;
-					this.visual.visual_type = 'tv';
-				}
-				if (douban.countries) {
-					this.visual.countries = douban.countries;
-				}
-				if (douban.languages) {
-					this.visual.languages = douban.languages;
-				}
-				if (douban.durations && douban.durations.length > 0) {
-					const duration = douban.durations[0];
-					this.visual.duration = duration.slice(0,duration.length - 2);
-				}
-				this.visual.website = douban.website;
-			}, res => {
-				//error
-			});
-		},
-		getImdbId() {
-			const options = {
-				params: {
-					douban_id: this.visual.douban_id
-				}
-			};
-			// const api = this.$store.state.api.visualImdb;
-			const api = '/api/visuals/get_imdb_id';
-			this.$http.get(api, options).then(res => {
-				if (res.body.imdb_id) {
-					this.visual.imdb_id = res.body.imdb_id;    
-				}
-				// this.release_dates = res.body.release_dates;
-				this.renderIMDB();
-			}, res => {
-					
-			});
-		},
-		renderIMDB() {
-			if (this.visual.imdb_id == '') return;
-			const options = {
-				params: {
-					i: this.visual.imdb_id,
-					apikey: '6ad10fa5'
-				}
-			};
-			this.$http.jsonp('https://www.omdbapi.com/', options).then(res => {
-				if (res.body.Response != 'False') {
-					if (res.body.Ratings[1] && res.body.Ratings[1].Source == 'Rotten Tomatoes') {
-						this.visual.rotten_rating = res.body.Ratings[1].Value.replace('%', '');   
-					}
-					this.posters.push(res.body.Poster);
-				}
-			});
-			const api = '/api/visuals/get_imdb_rating?imdb_id='+this.visual.imdb_id;
-			// 'https://what-i-watched.herokuapp.com/api/get_imdb_detail?imdb_id='+this.visual.imdb_id
-			this.$http.get(api).then(res => {
-				if (res.status == 200) {
-					if (res.body.imdb_rating) {
-						this.visual.imdb_rating = res.body.imdb_rating;
-					}
-				}
-			});
-		},
-		getSongs(visual_id) {
-			this.$http.get(this.$store.state.api.songList + '?visual_id=' + visual_id).then((res) => {
-				if (res.status == 200) {
-					this.songs = res.body.results;
-				}
-			});
-		},
-		editSong(s) {
-			let visual_id = this.$route.params.id;
-			this.$router.push({ path: '/song/' + s.id + '/edit' });
-		}
-	}
+  data() {
+    return {
+      currentField:'douban_id',
+      currentVal:'',
+      posters: [],
+      searchOpen: false,
+      searchs: [],
+      visual: {
+        id: 0,
+        title: '',
+        original_title: '',
+        douban_id: '',
+        douban_rating: '0.0',
+        imdb_id: '',
+        imdb_rating: '0.0',
+        rotten_id: '',
+        rotten_rating: 0,
+        rotten_audience_rating: 0,
+        release_date: '',
+        poster: '',
+        summary: '',
+        online_source: '',
+        episodes: 1,
+        current_episode: 0,
+        visual_type: 'movie',
+        website: '',
+        languages:[],
+        countries:[],
+        duration:0,
+      },
+      release_dates: [],
+      songs: []
+    };
+  },
+  mounted() {
+    const id = this.$route.params.id;
+    if (typeof id != 'undefined') {
+      this.getVisual(id);
+      this.getSongs(id);
+    }
+  },
+  methods: {
+    editField(k){
+      this.currentField = k;
+      this.currentVal = this.visual[k];
+    },
+    updateField(k) {
+      this.visual[k] = this.currentVal;
+      if (k == 'douban_id') {
+        this.renderDouban();
+      }
+    },
+    // toggleSearch() {
+    // 	this.searchOpen = !this.searchOpen;
+    // },
+    gotoAddSong() {
+      this.$router.push({path: '/' + this.$route.params.id + '/song/add'});
+    },
+    // gotoAddImage() {
+    // 	this.$router.push({path: '/' + this.$route.params.id + '/image/add'});
+    // },
+    renderFromSearch(id) {
+      this.toggleSearch();
+      this.visual.douban_id = id;
+      this.renderDouban();
+    },
+    handleSubmit() {
+      const options = this.visual;
+      this.$http.post(this.$store.state.api.visualSubmit, options).then(res => {
+        if (res.status == 200) {
+          if (res.body.code == 'exist') {
+            alert('Douban Id exist');
+          } else {
+            this.$router.push({ path: '/' });
+          }
+        }
+      });
+    },
+    getVisual(id) {
+      this.$http.get(this.$store.state.api.visualDetail + id).then(res => {
+        this.visual = res.body.result;
+        if (this.visual.douban_id) {
+          this.renderDouban();
+        }
+      });
+    },
+    // searchDouban(e) {
+    // 	const val = e.target.value;
+    // 	const api = '/api/visuals/search?keyword=' + val;
+    // 	// 'https://api.douban.com/v2/movie/search?q=' + val + '&apikey=0df993c66c0c636e29ecbb5344252a4a'
+    // 	this.$http.get(api).then(res => {
+    // 		this.searchs = [];
+    // 		if (res.status == 200) {
+    // 			console.log(res.body);
+    // 			this.searchs = res.body;
+    // 		}
+    // 	});
+    // },
+    renderDouban() {
+      if (this.visual.douban_id == '') {
+        alert('Empty douban id');
+        return;
+      }
+      this.getImdbId();
+      this.$http.jsonp('https://api.douban.com/v2/movie/subject/' + this.visual.douban_id + '?apikey=0df993c66c0c636e29ecbb5344252a4a').then(res => {
+        const douban = res.body;
+        if (this.visual.summary == '') {
+          this.visual.summary = douban.summary;
+        }
+        this.release_dates = douban.pubdates;
+        this.posters = [];
+        if (this.visual.poster) {
+          this.posters.push(this.visual.poster);
+        }
+        this.posters.push(douban.images.large);
+        
+        this.visual.title = douban.title;
+        if (!this.visual.original_title) {
+          this.visual.original_title = douban.original_title;
+        }
+        this.visual.douban_rating = douban.rating.average;
+        if (douban.episodes_count) {
+          this.visual.episodes = douban.episodes_count;
+          this.visual.visual_type = 'tv';
+        }
+        if (douban.countries) {
+          this.visual.countries = douban.countries;
+        }
+        if (douban.languages) {
+          this.visual.languages = douban.languages;
+        }
+        if (douban.durations && douban.durations.length > 0) {
+          const duration = douban.durations[0];
+          this.visual.duration = duration.slice(0,duration.length - 2);
+        }
+        this.visual.website = douban.website;
+      }, res => {
+        //error
+      });
+    },
+    getImdbId() {
+      const options = {
+        params: {
+          douban_id: this.visual.douban_id
+        }
+      };
+      // const api = this.$store.state.api.visualImdb;
+      const api = '/api/visuals/get_imdb_id';
+      this.$http.get(api, options).then(res => {
+        if (res.body.imdb_id) {
+          this.visual.imdb_id = res.body.imdb_id;    
+        }
+        // this.release_dates = res.body.release_dates;
+        this.renderIMDB();
+      }, res => {
+          
+      });
+    },
+    renderIMDB() {
+      if (this.visual.imdb_id == '') return;
+      const options = {
+        params: {
+          i: this.visual.imdb_id,
+          apikey: '6ad10fa5'
+        }
+      };
+      this.$http.jsonp('https://www.omdbapi.com/', options).then(res => {
+        if (res.body.Response != 'False') {
+          if (res.body.Ratings[1] && res.body.Ratings[1].Source == 'Rotten Tomatoes') {
+            this.visual.rotten_rating = res.body.Ratings[1].Value.replace('%', '');   
+          }
+          this.posters.push(res.body.Poster);
+        }
+      });
+      const api = '/api/visuals/get_imdb_rating?imdb_id='+this.visual.imdb_id;
+      // 'https://what-i-watched.herokuapp.com/api/get_imdb_detail?imdb_id='+this.visual.imdb_id
+      this.$http.get(api).then(res => {
+        if (res.status == 200) {
+          if (res.body.imdb_rating) {
+            this.visual.imdb_rating = res.body.imdb_rating;
+          }
+        }
+      });
+    },
+    getSongs(visual_id) {
+      this.$http.get(this.$store.state.api.songList + '?visual_id=' + visual_id).then((res) => {
+        if (res.status == 200) {
+          this.songs = res.body.results;
+        }
+      });
+    },
+    editSong(s) {
+      let visual_id = this.$route.params.id;
+      this.$router.push({ path: '/song/' + s.id + '/edit' });
+    }
+  }
 };
 </script>
 <style scope>
