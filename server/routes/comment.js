d085afc60376c828db8094a0d19ce949be386d18
Sam Li <weisen.li@hotmail.com>
2020-05-08 21:43:39 -0400

    reduce space on comment route

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -4,25 +4,24 @@ var router = express.Router();
 const Comment = require('../models/comment');
 
 router.get('/', (req, res, next) => {
-    Comment.find({}).exec((err, comments) => {
-        if (err) throw err;
-        res.send(comments);
-    });
+  Comment.find({}).exec((err, comments) => {
+    if (err) throw err;
+    res.send(comments);
+  });
 });
 
 router.post('/', (req, res, next) => {
+  const name = req.body.name;
+  const content = req.body.content;
+  const comment = Comment({
+    name: name,
+    content: content
+  });
 
-    const name = req.body.name;
-    const content = req.body.content;
-    const comment = Comment({
-        name: name,
-        content: content
-    });
-  
-    comment.save((err) => {
-        if (err) throw err;
-        res.send(comment);
-    });
+  comment.save((err) => {
+    if (err) throw err;
+    res.send(comment);
+  });
 });
 
 module.exports = router;
