d648671eaac263a333153d6e2cc746440a6bf0a6
Sam Li <weisen.li@hotmail.com>
2020-05-10 18:23:53 -0400

    initial movie detail page

--- client/resume/src/pages/Movie.jsx
+++ client/resume/src/pages/Movie.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import {Box, BoxTitle, BoxBody} from '../components/style.jsx';
 
-export default class Movie extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<h1>Hello Movie</h1>
-		);
-	}
+const Movie = (props) => {
+  console.log(props.match.params.id);
+  const [visual,setVisual] = useState({id:props.match.params.id});
+  const [loading,setLoading] = useState(true);
+  useEffect(()=>{
+    axios.get(`https://what-i-watched.herokuapp.com/api/visual/${visual.id}`).then((res) => {
+      setVisual(res.data.result);
+      setLoading(false);
+    });
+  },[]);
+  return (
+    <Box className="movies">
+      <BoxTitle>
+        <i className="boxIcon fa fa-film" aria-hidden="true"></i>
+        <span>Movie Title</span>
+      </BoxTitle>
+      <BoxBody>
+        <div className="visual__detail">
+          {loading ?
+          <div className="lds-hourglass"></div>:
+          <div>{visual.title}</div>
+          }
+        </div>
+      </BoxBody>
+    </Box>
+  );
 }
+export default Movie;
