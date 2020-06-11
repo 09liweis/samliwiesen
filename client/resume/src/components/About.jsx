002b06b55ec048d1e4e46eef5f8d50f9ee011475
Sam Li <weisen.li@hotmail.com>
2020-06-10 21:12:36 -0400

    tab to space about.jsx

--- client/resume/src/components/About.jsx
+++ client/resume/src/components/About.jsx
@@ -3,19 +3,19 @@ import styled from 'styled-components';
 import {Box, BoxTitle, BoxBody} from './style.jsx';
 
 const About = () => {
-	const title = 'developer'.split('').map((t,i)=>
-		<span key={i}>{t}</span>
-	);
-	return (
-		<Box className="experiences">
-			<BoxTitle>
-				<i className="boxIcon fa fa-user" aria-hidden="true"></i>
-				<span>Hi I am Sam</span>
-			</BoxTitle>
-			<BoxBody>
-				<div id="about">I am a {title}</div>
-			</BoxBody>
-		</Box>
-	);
+  const title = 'developer'.split('').map((t,i)=>
+    <span key={i}>{t}</span>
+  );
+  return (
+    <Box className="experiences">
+      <BoxTitle>
+        <i className="boxIcon fa fa-user" aria-hidden="true"></i>
+        <span>Hi I am Sam</span>
+      </BoxTitle>
+      <BoxBody>
+        <div id="about">I am a {title}</div>
+      </BoxBody>
+    </Box>
+  );
 };
 export default About;
