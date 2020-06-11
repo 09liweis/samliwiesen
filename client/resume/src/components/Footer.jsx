c0bd0bff2c2527ff5fed255eccc6b7058a604ffb
Sam Li <weisen.li@hotmail.com>
2020-06-10 21:13:42 -0400

    tab to space footer.jsx

--- client/resume/src/components/Footer.jsx
+++ client/resume/src/components/Footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
-	background-color: #06A763;
-	padding: 30px 0;
-	color: #ffffff;
-	text-align: center;
-	margin-top: 30px;
+  background-color: #06A763;
+  padding: 30px 0;
+  color: #ffffff;
+  text-align: center;
+  margin-top: 30px;
 `;
 
 const Footer = () => 
-	<FooterContainer>
-		Made with React and By Sam Li © {new Date().getFullYear()}
-	</FooterContainer>;
+  <FooterContainer>
+    Made with React and By Sam Li © {new Date().getFullYear()}
+  </FooterContainer>;
 export default Footer;
