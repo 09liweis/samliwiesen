fd0d1f6c6d9105e707f887aaf8c188034b5727ee
Sam Li <weisen.li@hotmail.com>
2020-06-15 16:59:19 -0400

    tab to space projects.jsx

--- client/resume/src/components/home/Projects.jsx
+++ client/resume/src/components/home/Projects.jsx
@@ -6,47 +6,42 @@ import styled from 'styled-components';
 import {Box, BoxTitle, BoxBody} from '../style.jsx';
 
 const Link = styled.a`
-	color: #000000;
-	text-decoration: none;
-	transition: 0.2s ease-in;
-	&:hover {
-		color: #06A763;
-	}
+  color: #000000;
+  text-decoration: none;
+  transition: 0.2s ease-in;
+  &:hover {
+    color: #06A763;
+  }
 `;
 const Projects = () => {
 // const Projects = ({projects}) => {
-	const projects = useSelector(state => state.projects);
-	const pros = projects.map((p, i) => {
-		const steps = p.steps.map((s, j) => {
-			return (
-				<li key={j}>{s}</li>
-			);
-		});
-		return (
-			<div className="project" key={i}>
-				<h3 className="project__title">
-					<Link target="_blank" rel="noopener" href={p.link}><i className="fa fa-link" aria-hidden="true"></i>{p.name}</Link> - <span className="project__type">{p.type}</span></h3>
-				<ul className="list">
-					{steps}
-				</ul>
-			</div>
-		);
-	});
-	return(
-		<Box className="projects">
-			<BoxTitle>
-				<i className="boxIcon fa fa-tasks" aria-hidden="true"></i>
-				<span>Projects</span>
-			</BoxTitle>
-			<BoxBody>
-			{pros}
-			</BoxBody>
-		</Box>
-	);
+  const projects = useSelector(state => state.projects);
+  const pros = projects.map((p, i) => {
+    const steps = p.steps.map((s, j) => {
+      return (
+        <li key={j}>{s}</li>
+      );
+    });
+    return (
+      <div className="project" key={i}>
+        <h3 className="project__title">
+          <Link target="_blank" rel="noopener" href={p.link}><i className="fa fa-link" aria-hidden="true"></i>{p.name}</Link> - <span className="project__type">{p.type}</span></h3>
+        <ul className="list">
+          {steps}
+        </ul>
+      </div>
+    );
+  });
+  return(
+    <Box className="projects">
+      <BoxTitle>
+        <i className="boxIcon fa fa-tasks" aria-hidden="true"></i>
+        <span>Projects</span>
+      </BoxTitle>
+      <BoxBody>
+      {pros}
+      </BoxBody>
+    </Box>
+  );
 };
 export default Projects;
-// const mapStateToProps = ({projects}) => ({
-//   projects
-// });
-
-// export default connect(mapStateToProps)(Projects);
