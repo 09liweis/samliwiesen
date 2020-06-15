945cc8c43a97c2ef011d339bf518831671245599
Sam Li <weisen.li@hotmail.com>
2020-06-15 17:00:21 -0400

    tab to space experiences.jsx

--- client/resume/src/components/home/Experiences.jsx
+++ client/resume/src/components/home/Experiences.jsx
@@ -4,46 +4,40 @@ import {getExperiences} from '../../actions/experience';
 
 import {Box, BoxTitle, BoxBody, ExperienceDate} from '../style.jsx';
 
-// const Experiences = ({experiences}) => {
 const Experiences = () => {
-	const dispatch = useDispatch();
-	const experiences = useSelector(state => state.experiences);
-	if (experiences.length == 0) {
-		dispatch(getExperiences());
-	}
-	const exs = experiences.map((e, i) => {
-		const duties = e.duty.map((d, j) => {
-			return (
-				<li key={j}>
-					{d}
-				</li>
-			);
-		});
-		return (
-			<div className="experience" key={i}>
-				<h3>{e.company} - {e.title}</h3>
-		<ExperienceDate><i className="boxIcon fa fa-calendar" aria-hidden="true"></i>{e.start_date} - {e.end_date}</ExperienceDate>
-				<ul className="list">
-					{duties}
-				</ul>
-			</div>
-		);
-	});
-	return(
-		<Box className="experiences">
-				<BoxTitle>
-					<i className="boxIcon fa fa-briefcase" aria-hidden="true"></i>
-					<span>Experiences</span>
-				</BoxTitle>
-				<BoxBody>
-				{exs}
-				</BoxBody>
-		</Box>
-	);
+  const dispatch = useDispatch();
+  const experiences = useSelector(state => state.experiences);
+  if (experiences.length == 0) {
+    dispatch(getExperiences());
+  }
+  const exs = experiences.map((e, i) => {
+    const duties = e.duty.map((d, j) => {
+      return (
+        <li key={j}>
+          {d}
+        </li>
+      );
+    });
+    return (
+      <div className="experience" key={i}>
+        <h3>{e.company} - {e.title}</h3>
+    <ExperienceDate><i className="boxIcon fa fa-calendar" aria-hidden="true"></i>{e.start_date} - {e.end_date}</ExperienceDate>
+        <ul className="list">
+          {duties}
+        </ul>
+      </div>
+    );
+  });
+  return(
+    <Box className="experiences">
+        <BoxTitle>
+          <i className="boxIcon fa fa-briefcase" aria-hidden="true"></i>
+          <span>Experiences</span>
+        </BoxTitle>
+        <BoxBody>
+        {exs}
+        </BoxBody>
+    </Box>
+  );
 };
 export default Experiences;
-// const mapStateToProps = ({experiences}) => ({
-//   experiences
-// });
-
-// export default connect(mapStateToProps)(Experiences);
