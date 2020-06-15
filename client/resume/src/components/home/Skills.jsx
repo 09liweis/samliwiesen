e2ab118937a491faa427b51deb2eab31b2d37a76
Sam Li <weisen.li@hotmail.com>
2020-06-15 16:58:41 -0400

    tab to space skills.jsx

--- client/resume/src/components/home/Skills.jsx
+++ client/resume/src/components/home/Skills.jsx
@@ -5,39 +5,39 @@ import styled from 'styled-components';
 import {Box, BoxTitle, BoxBody} from '../style.jsx';
 
 const Skill = styled.span`
-	background-color: ${props => props.bg};
-	padding: 6px;
-	color: #ffffff;
-	margin-right: 5px;
-	margin-bottom: 5px;
-	font-size: 12px;
-	display: inline-block;
+  background-color: ${props => props.bg};
+  padding: 6px;
+  color: #ffffff;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  font-size: 12px;
+  display: inline-block;
 `;
 
 // const Skills = ({skills}) => {
 const Skills = () => {
-	const skills = useSelector(state => state.skills);
-	const sks = skills.map((s, i) => {
-		const techs = s.techs.map((t, j) => 
-			<Skill bg={t.color} key={j}>{t.name}</Skill>
-		);
-		return (
-			<div className="skills__group" key={i}>
-				<h3 className="skills__title">{s.category}</h3>
-				{techs}
-			</div>
-		);
-	});
-	return(
-		<Box className="skills">
-			<BoxTitle>
-				<i className="boxIcon fa fa-pencil" aria-hidden="true"></i>
-				<span>Skills</span>
-			</BoxTitle>
-			<BoxBody>
-			{sks}
-			</BoxBody>
-		</Box>
-	);
+  const skills = useSelector(state => state.skills);
+  const sks = skills.map((s, i) => {
+    const techs = s.techs.map((t, j) => 
+      <Skill bg={t.color} key={j}>{t.name}</Skill>
+    );
+    return (
+      <div className="skills__group" key={i}>
+        <h3 className="skills__title">{s.category}</h3>
+        {techs}
+      </div>
+    );
+  });
+  return(
+    <Box className="skills">
+      <BoxTitle>
+        <i className="boxIcon fa fa-pencil" aria-hidden="true"></i>
+        <span>Skills</span>
+      </BoxTitle>
+      <BoxBody>
+      {sks}
+      </BoxBody>
+    </Box>
+  );
 };
 export default Skills;
