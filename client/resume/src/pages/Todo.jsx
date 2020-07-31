b23187e5a077173348c0fe5f2e2ffc0246099b23
Sam Li <weisen.li@hotmail.com>
2020-07-31 12:19:16 -0400

    base html on todo.jsx

--- client/resume/src/pages/Todo.jsx
+++ client/resume/src/pages/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector,useDispatch } from 'react-redux';
 import axios from 'axios';
 
 import {CSSTransition,TransitionGroup} from 'react-transition-group';
@@ -7,6 +8,28 @@ import '../css/todo.css';
 const API = '/api/todos/';
 
 const Todo = () => {
-
+  var form,todoList,errorMsg,loading;
+  return (
+    <div className="container">
+      {form}
+      <h2 className="todos__title">Todo List</h2>
+      <div className="todos__container">
+        <a className="todo__btn">Add New</a>
+        <div className="todo__statics">
+          <span className="done">Done</span>
+          <span className="working">In Progress</span>
+          <span className="pending">Pending</span>
+        </div>
+        {loading ? 
+        <div className="todos__loader"></div>
+        :
+        <TransitionGroup>
+        {todoList}
+        </TransitionGroup>
+        }
+        {errorMsg}
+      </div>
+    </div>
+  )
 }
 export default Todo;
