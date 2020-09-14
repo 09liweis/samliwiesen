7a2774fc54abe6f57f511cde5fb02a24a2ddbeeb
Sam Li <weisen.li@hotmail.com>
2020-09-14 16:56:56 -0400

    style movie detail page poster

--- client/resume/src/pages/Movie.jsx
+++ client/resume/src/pages/Movie.jsx
@@ -1,30 +1,53 @@
 import React, {useState,useEffect} from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 import {Box, BoxTitle, BoxBody} from '../components/style.jsx';
 
+const Poster = styled.img`
+  width: 180px;
+  display: block;
+  margin: auto;
+  border-radius: 10px;
+`;
+
 const Movie = (props) => {
-  console.log(props.match.params.id);
   const [visual,setVisual] = useState({id:props.match.params.id});
   const [loading,setLoading] = useState(true);
+  function getSummary(douban_id,cb) {
+    axios.post('/api/visuals/summary',{douban_id}).then((res) => {
+      console.log(res);
+      return cb(res);
+    });
+  }
   useEffect(()=>{
     axios.get(`https://what-i-watched.herokuapp.com/api/visual/${visual.id}`).then((res) => {
-      setVisual(res.data.result);
-      setLoading(false);
+      const {douban_id} = res.data.result;
+      let v = res.data.result;
+      getSummary(douban_id,(res)=> {
+        if (res && res.douban_rating) {
+          v = Object.assign(v,res);
+        }
+        setVisual(v);
+        setLoading(false);
+      });
     });
   },[]);
   return (
     <Box className="movies">
       <BoxTitle>
         <i className="boxIcon fa fa-film" aria-hidden="true"></i>
-        <span>Movie Title</span>
+        <span>{'Movie Title' || visual.title }</span>
       </BoxTitle>
       <BoxBody>
-        <div className="visual__detail">
-          {loading ?
-          <div className="lds-hourglass"></div>:
-          <div>{visual.title}</div>
-          }
+        {loading ?
+        <div className="">Loading</div>:
+        <div style={{position:'relative'}}>
+          <Poster src={visual.poster} />
+          <div style={{marginTop: '-20px'}}>
+            <div className="visual__rating">{visual.douban_rating}</div>
+          </div>
         </div>
+        }
       </BoxBody>
     </Box>
   );
