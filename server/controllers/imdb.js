42dc227a1edb36745a39c376a19f118fa88ad634
Sam Li <weisen.li@hotmail.com>
2020-09-29 14:22:39 -0400

    initial movies list in imdb_boxoffice api

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -5,9 +5,21 @@ const IMDB_BOXOFFICE = 'https://www.imdb.com/chart/boxoffice';
 exports.getImdbBoxOffice = (req,resp) => {
   sendRequest(IMDB_BOXOFFICE,'GET',resp,(statusCode, body) => {
     const $ = getCheerio(body);
+    const chart = $('table.chart tbody tr');
+    let movies = [];
+    if (chart) {
+      for (let i = 0; i < chart.length; i++) {
+        const movie = $(chart[i]);
+        movies.push({
+          title: movie.find('.titleColumn a').text(),
+          poster: movie.find('.posterColumn img').attr('src')
+        });
+      }
+    }
     let boxOffice = {
       title: $('h1.header').text(),
-      date: $('#boxoffice h4').text()
+      date: $('#boxoffice h4').text(),
+      movies
     }
     resp.status(statusCode).json(boxOffice);
   });
