c41580f339b12d5d8ff50f66443900a46d3952d3
Sam Li <weisen.li@hotmail.com>
2020-09-29 14:17:47 -0400

    add date to imdb_boxoffice api

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -6,7 +6,8 @@ exports.getImdbBoxOffice = (req,resp) => {
   sendRequest(IMDB_BOXOFFICE,'GET',resp,(statusCode, body) => {
     const $ = getCheerio(body);
     let boxOffice = {
-      title: $('h1.header').text()
+      title: $('h1.header').text(),
+      date: $('#boxoffice h4').text()
     }
     resp.status(statusCode).json(boxOffice);
   });
