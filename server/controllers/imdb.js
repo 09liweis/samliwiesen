b5f0731fa1d6140fdeac48891620cd36f16f82ce
Sam Li <weisen.li@hotmail.com>
2020-09-29 14:15:58 -0400

    get boxoffice title in imdb_boxoffice api

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -1,5 +1,13 @@
 const {sendRequest,getCheerio} = require('../helpers/request');
 
+const IMDB_BOXOFFICE = 'https://www.imdb.com/chart/boxoffice';
+
 exports.getImdbBoxOffice = (req,resp) => {
-  resp.status(200).json({ok:1});
+  sendRequest(IMDB_BOXOFFICE,'GET',resp,(statusCode, body) => {
+    const $ = getCheerio(body);
+    let boxOffice = {
+      title: $('h1.header').text()
+    }
+    resp.status(statusCode).json(boxOffice);
+  });
 }
