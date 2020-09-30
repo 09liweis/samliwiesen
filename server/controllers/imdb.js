095c4717006880f77013d51f9691554511e8067c
Sam Li <weisen.li@hotmail.com>
2020-09-30 10:31:25 -0400

    use return value $ in getImdbBoxOffice

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -3,8 +3,7 @@ const {sendRequest,getCheerio} = require('../helpers/request');
 const IMDB_BOXOFFICE = 'https://www.imdb.com/chart/boxoffice';
 
 exports.getImdbBoxOffice = (req,resp) => {
-  sendRequest(IMDB_BOXOFFICE,'GET',resp,(statusCode, body) => {
-    const $ = getCheerio(body);
+  sendRequest(IMDB_BOXOFFICE,'GET',resp,(statusCode, $) => {
     const chart = $('table.chart tbody tr');
     let movies = [];
     if (chart) {
