dc5d7ab30f10ee12db48bf9797b7da457eb76d72
Sam Li <weisen.li@hotmail.com>
2020-10-02 08:34:39 -0400

    fix getToday function

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,8 +1,8 @@
 function getToday() {
   const now = new Date();
   const year = now.getFullYear();
-  const month = now.getMonth();
-  const m = month > 9 ? month : `0${month + 1}`;
+  const month = now.getMonth() + 1;
+  const m = (month) >= 10 ? month : `0${month + 1}`;
   const day = now.getDate();
   const d = day > 9 ? day : `0${day}`;
   return {year,m,d}
