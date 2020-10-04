a1d7d62d5785018ef7566a44f3a7d0a4bc92f0a1
Sam Li <weisen.li@hotmail.com>
2020-10-03 23:28:01 -0400

    style transaction component

--- client/resume/src/components/Transaction.jsx
+++ client/resume/src/components/Transaction.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TransactionItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   position: relative;
   background-color: #ffffff;
   padding: 10px;
-  margin: 0 10px 20px;
+  margin: 0 10px 10px;
   border-radius: 3px;
   @media (min-width: 768px) {
     width: 46%;
@@ -20,7 +23,7 @@ const TransactionDelete = styled.div`
 `;
 export default class Transaction extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
   }
   render() {
     const t = this.props.t;
@@ -30,7 +33,8 @@ export default class Transaction extends React.Component {
     return (
       <TransactionItem className="box-shadow">
         <div className="transaction__info">
-          <div className="transaction__title">{t.title + ' - ' + t.category}</div>
+          <div className="transaction__title">{t.title}</div>
+          <div className="transaction__category">{t.category}</div>
           <div className="transaction__date">{t.date}</div>
         </div>
         <div className={priceClass}>${Math.abs(t.price)}</div>
