4a9aa7f9bae82a850e3c9fcba9d1e0c7d614db21
Sam Li <weisen.li@hotmail.com>
2020-11-12 21:49:37 -0500

    add douban tag api

--- server/routes/visual.js
+++ server/routes/visual.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const VisualController = require('../controllers/visual.js');
 const {getImdbBoxOffice} = require('../controllers/imdb.js');
-const {getSubjects} = require('../controllers/douban.js');
+const {getSubjects,getTags} = require('../controllers/douban.js');
 
 router.route('/search').post(VisualController.search);
 
@@ -24,5 +24,7 @@ router.route('/reviews').post(VisualController.getReviews);
 router.route('/imdb_boxoffice').post(getImdbBoxOffice);
 
 router.route('/douban').post(getSubjects);
+
+router.route('/douban/tags').post(getTags);
 
 module.exports = router;
