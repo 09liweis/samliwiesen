3ff4e016c3ce232a12529cce3f0a192355dc2f2f
Sam Li <weisen.li@hotmail.com>
2020-11-13 20:48:29 -0500

    use getPhotos in photos api

--- server/routes/visual.js
+++ server/routes/visual.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const VisualController = require('../controllers/visual.js');
 const {getImdbBoxOffice} = require('../controllers/imdb.js');
-const {getSubjects,getTags} = require('../controllers/douban.js');
+const {getSubjects,getTags,getPhotos} = require('../controllers/douban.js');
 
 router.route('/search').post(VisualController.search);
 
@@ -13,7 +13,7 @@ router.route('/celebrities').post(VisualController.getCelebrities);
 
 router.route('/summary').post(VisualController.getSummary);
 
-router.route('/photos').post(VisualController.getPhotos);
+router.route('/photos').post(getPhotos);
 
 router.route('/photo').post(VisualController.getPhotoDetail);
 
