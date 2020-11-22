ac642b3d47da955690899ef5698ffb8d3aba58b1
Sam Li <weisen.li@hotmail.com>
2020-11-21 22:49:31 -0500

    add bcrypt to hashpassword

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,12 +15,21 @@ exports.register = async (req,res)=>{
     msg = 'Email is taken';
     return res.status(400).json({msg});
   } else {
-    user = new User(body);
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(pwd,salt);
+    user = new User({
+      eml,
+      nm,
+      pwd: hashPassword,
+      lts: new Date()
+    });
     await user.save()
+    msg = 'Register done'
     res.status(200).json({msg});
   }
 }
 exports.login = (req, resp) => {
+  const {eml,nm,pwd} = req.body;
   
 }
 function handleError(res, err) {
