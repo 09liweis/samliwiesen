d372bfd43aaae8de49049abaa83400467455951b
Sam Li <weisen.li@hotmail.com>
2020-11-21 23:30:40 -0500

    create verifyToken js

---
+++ server/helpers/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const {token_secret} = require('./constant');
+
+exports.verify = (req,resp,next) => {
+  const token = req.header('auth-token');
+  if (!token) {
+    return resp.status(400).json({msg:'Access Denied'});
+  }
+  try {
+    const verified = jwt.verify(token, token_secret);
+    req.user = verified;
+  } catch (error) {
+    resp.status(400).json({msg:'Invalid token'});
+  }
+}
+exports.sign = (data) => {
+  const token = jwt.sign(data,token_secret);
+  return token;
+}
