5802b64e93eaf8c84307e67b0dd1fc4a9f01843a
Sam Li <weisen.li@hotmail.com>
2020-11-29 16:58:15 -0500

    add DOUBAN_SITE_API

--- server/helpers/douban.js
+++ server/helpers/douban.js
@@ -1,5 +1,7 @@
 const DOUBAN_SITE = 'https://movie.douban.com/subject/';
 
+exports.DOUBAN_SITE_API = 'https://movie.douban.com/j/';
+
 exports.getDoubanUrl = (douban_id,opt={}) => {
   let apiName = '';
   if (opt && opt.apiName) {
