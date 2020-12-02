c58b4557b6c38b936af2a29826180ca20f39fa2e
Sam Li <weisen.li@hotmail.com>
2020-12-02 10:00:13 -0500

    add movie detail server render page

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,9 @@ router.route('/').get(function(req, res) {
   res.status(200).json('Resume site comming soon');
 });
 
+router.route('/movie/:id').get((req,resp) => {
+  let douban_id = req.params.id;
+  resp.json('movie detail '+douban_id);
+});
+
 module.exports = router;
