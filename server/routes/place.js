1e0461efe97a310774f8af238ca66708ed050ebd
Sam Li <weisen.li@hotmail.com>
2020-12-04 23:39:36 -0500

    refactor place method in place route

--- server/routes/place.js
+++ server/routes/place.js
@@ -1,18 +1,18 @@
 var express= require('express');
 var router = express.Router();
 
-const PlaceController = require('../controllers/place.js');
+const {place_list,place_update_random,place_upsert,place_detail,search_google} = require('../controllers/place.js');
 
 router.route('/')
-.get(PlaceController.place_list)
-.post(PlaceController.place_upsert);
+.get(place_list)
+.post(place_upsert);
 
-router.route('/update_random').get(PlaceController.place_update_random);
+router.route('/update_random').get(place_update_random);
 
 router.route('/:id')
-.get(PlaceController.place_detail);
+.get(place_detail);
 
 router.route('/search_google')
-.post(PlaceController.search_google);
+.post(search_google);
 
 module.exports = router;
