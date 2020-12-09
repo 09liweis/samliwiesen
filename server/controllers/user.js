eee20e549217c30b3c5510250e7983fe2d3b3d88
Sam Li <weisen.li@hotmail.com>
2020-12-09 09:49:53 -0500

    update user list fields

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const {sign} = require('../helpers/verifyToken');
 
 exports.list = (req, res) => {
-  User.find({}, '_id title url content image category published created_at').sort('-created_at').exec((err, users) => {
+  User.find({}, '_id eml lts created_at').sort('-created_at').exec((err, users) => {
     handleError(res, err);
     res.json(users);
   });
