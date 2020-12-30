ad20f47f154529c38ee557595787c4fa76d36f2b
Sam Li <weisen.li@hotmail.com>
2020-12-30 13:53:30 -0500

    add buildHttpOptions to constant

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,3 +1,5 @@
+import { HttpHeaders } from '@angular/common/http';
+
 const API = 'https://samliweisen.herokuapp.com/api/';
 // const API = 'http://localhost:8081/api/'
 function genAPI(endpoint) {
@@ -9,7 +11,20 @@ function getAuthToken() {
   return localStorage.getItem('auth-token');
 }
 
+function buildHttpOptions() {
+  var authToken = getAuthToken();
+  var headers = new HttpHeaders();
+  headers = headers.set('Content-Type','application/json');
+  if (authToken) {
+    headers = headers.set('auth-token',authToken);
+  }
+  return {
+    headers
+  };
+}
+
 export {
   genAPI,
-  getAuthToken
+  getAuthToken,
+  buildHttpOptions
 };
