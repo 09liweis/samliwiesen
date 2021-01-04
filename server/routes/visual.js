66fc59b71266fce8e77cb69431aaaa3a26331a1c
Sam Li <weisen.li@hotmail.com>
2021-01-04 14:10:03 -0500

    refactor visual route methods

--- server/routes/visual.js
+++ server/routes/visual.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const VisualController = require('../controllers/visual.js');
+const {search,inTheatre,getCelebrities,getSummary,getPhotoDetail,getComments,getReviews} = require('../controllers/visual.js');
 const {getImdbBoxOffice} = require('../controllers/imdb.js');
 const {getSubjects,getTags,getPhotos,getCast} = require('../controllers/douban.js');
 
-router.route('/search').post(VisualController.search);
+router.route('/search').post(search);
 
-router.route('/in_theatre').post(VisualController.inTheatre);
+router.route('/in_theatre').post(inTheatre);
 
-router.route('/celebrities').post(VisualController.getCelebrities);
+router.route('/celebrities').post(getCelebrities);
 
 router.route('/cast').post(getCast);
 
-router.route('/summary').post(VisualController.getSummary);
+router.route('/summary').post(getSummary);
 
 router.route('/photos').post(getPhotos);
 
-router.route('/photo').post(VisualController.getPhotoDetail);
+router.route('/photo').post(getPhotoDetail);
 
-router.route('/comments').post(VisualController.getComments);
+router.route('/comments').post(getComments);
 
-router.route('/reviews').post(VisualController.getReviews);
+router.route('/reviews').post(getReviews);
 
 router.route('/imdb_boxoffice').post(getImdbBoxOffice);
 
