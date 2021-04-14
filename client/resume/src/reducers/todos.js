98696ec88263366daeeb7e333eb15f570d8315e6
Sam Li <weisen.li@hotmail.com>
2021-04-14 16:35:50 -0400

    add delete todo reducer

--- client/resume/src/reducers/todos.js
+++ client/resume/src/reducers/todos.js
@@ -3,7 +3,12 @@ const todos = (state = {items:[],loading:true}, action) => {
     case 'GET_TODOS':
       return {items:action.ex,loading:false};
     case 'ADD_TODO':
-      return {items:[action.ex,...state.items]};
+      var items = [action.ex,...state.items];
+      return {items};
+    case 'DELETE_TODO':
+      var idx = action.idx;
+      state.items.splice(idx,1);
+      return  {items:state.items};
     default:
       return state;
   }
