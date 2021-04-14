4e329fb262776202e240333e072c3f46c48f39e2
Sam Li <weisen.li@hotmail.com>
2021-04-14 16:25:12 -0400

    ADD_TODO reducer state

--- client/resume/src/reducers/todos.js
+++ client/resume/src/reducers/todos.js
@@ -3,7 +3,7 @@ const todos = (state = {items:[],loading:true}, action) => {
     case 'GET_TODOS':
       return {items:action.ex,loading:false};
     case 'ADD_TODO':
-      return state;
+      return {items:[action.ex,...state.items]};
     default:
       return state;
   }
