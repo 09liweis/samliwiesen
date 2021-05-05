5232da29442a21ebda6fdbea88abba1026112976
Sam Li <weisen.li@hotmail.com>
2021-05-05 14:13:31 -0400

    refactor imdb box office

--- server/controllers/imdb.js
+++ server/controllers/imdb.js
@@ -8,29 +8,29 @@ exports.getImdbBoxOffice = (req,resp) => {
       return sendErr(resp,err);
     }
     const chart = $('table.chart tbody tr');
-    if (chart) {
-      var movies = chart.toArray().map((c)=>{
-        const movie = $(c);
-        const [weekend,gross] = movie.find('.ratingColumn').text().trim().split('  ');
-        var imdbUrl = movie.find('.posterColumn a').attr('href').split('?')[0];
-        if (imdbUrl) {
-          var imdb_id = imdbUrl.split('/')[2];
-        }
-        return {
-          imdb_id,
-          title: movie.find('.titleColumn a').text(),
-          poster: movie.find('.posterColumn img').attr('src'),
-          weekend,
-          gross,
-          weeks: movie.find('.weeksColumn').text()
-        };
-      });
-    }
     let boxOffice = {
       title: $('h1.header').text(),
-      date: $('#boxoffice h4').text(),
-      movies
+      date: $('#boxoffice h4').text()
     }
-    sendResp(resp,boxOffice);
+    if (!chart) {
+      return sendResp(resp, boxOffice);
+    }
+    boxOffice.movies = chart.toArray().map((c)=>{
+      const movie = $(c);
+      const [weekend,gross] = movie.find('.ratingColumn').text().trim().split('  ');
+      var imdbUrl = movie.find('.posterColumn a').attr('href').split('?')[0];
+      if (imdbUrl) {
+        var imdb_id = imdbUrl.split('/')[2];
+      }
+      return {
+        imdb_id,
+        title: movie.find('.titleColumn a').text(),
+        poster: movie.find('.posterColumn img').attr('src'),
+        weekend,
+        gross,
+        weeks: movie.find('.weeksColumn').text()
+      };
+    });
+    return sendResp(resp,boxOffice);
   });
 }
