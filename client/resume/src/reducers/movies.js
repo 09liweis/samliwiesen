08e4ecbb351b82ff1e5b1c2870ca16ef5f2086f8
Sam Li <weisen.li@hotmail.com>
2021-05-15 15:00:31 -0400

    update movie reducer

--- client/resume/src/reducers/movies.js
+++ client/resume/src/reducers/movies.js
@@ -1,10 +1,11 @@
 const movies = (state = {items:[],loading:true}, action) => {
   switch (action.type) {
     case 'GET_MOVIES':
-      return {items:action.ex,loading:false};
-    case 'GET_MORE_MOVIES':
-      var items = [action.ex,...state.items];
+      var items = state.items.concat(action.ex);
       return {items,loading:false};
+    // case 'GET_MORE_MOVIES':
+    //   var items = [action.ex,...state.items];
+    //   return {items,loading:false};
     default:
       return state;
   }
