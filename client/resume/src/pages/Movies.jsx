99302d5647afb8a54f88887e78d659d48c20541f
Sam Li <weisen.li@hotmail.com>
2021-05-15 15:00:21 -0400

    refactor movies to function

--- client/resume/src/pages/Movies.jsx
+++ client/resume/src/pages/Movies.jsx
@@ -1,61 +1,41 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import {getMovies} from '../actions/movie';
 import {Box, BoxTitle, BoxBody} from '../components/style.jsx';
 import Movie from '../components/Movie.jsx';
 
 import '../css/movies.css';
 
-export default class Movies extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visuals: [],
-      api: 'https://what-i-watched.herokuapp.com/api/visuals',
-      limit: 20,
-      loading: true
-    };
-    this.loadmore = this.loadmore.bind(this);
+const Movies = () => {
+  var [page,setPage] = useState(1);
+  const {items,loading} = useSelector(state => state.movies);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMovies({limit:15,page}));
+  },[]);
+  const handleLoadmore = () => {
+    page += 1;
+    setPage(page);
+    dispatch(getMovies({limit:15,page}));
   }
-  UNSAFE_componentWillMount() {
-    this.setState({loading: true});
-    this.page = 1;
-    this.getVisuals();
-  }
-  loadmore() {
-    this.page += 1;
-    this.getVisuals();
-  }
-  getVisuals() {
-    const {visuals, limit, api} = this.state;
-    axios.get(api, {params: {page: this.page, limit: limit}}).then((res) => {
-      if (res.status == 200) {
-        this.setState({
-          visuals: visuals.concat(res.data.results),
-          loading: false
-        });
-      }
-    });
-  }
-  render() {
-    const visuals = this.state.visuals.map((v) => {
-      return (
-        <Movie v={v} key={v.id} />
-      );
-    });
-    const loading = this.state.loading;
+  const visuals = items.map((v) => {
     return (
-      <Box className="movies">
-        <BoxTitle>
-          <i className="boxIcon fa fa-film" aria-hidden="true"></i>
-          <span>What I Watched</span>
-        </BoxTitle>
-        <BoxBody>
-          <div className="visual__list">
-            {loading ?<div className="lds-hourglass"></div>:visuals}
-          </div>
-          <div className="loadmore" onClick={this.loadmore}>Load More</div>
-        </BoxBody>
-      </Box>
+      <Movie v={v} key={v.id} />
     );
-  }
+  });
+  return (
+    <Box className="movies">
+      <BoxTitle>
+        <i className="boxIcon fa fa-film" aria-hidden="true"></i>
+        <span>What I Watched</span>
+      </BoxTitle>
+      <BoxBody>
+        <div className="visual__list">
+          {loading ?<div className="lds-hourglass"></div>:visuals}
+        </div>
+        <div className="visual__loadmore" onClick={()=>handleLoadmore()}>Load More</div>
+      </BoxBody>
+    </Box>
+  );
 }
+export default Movies;
