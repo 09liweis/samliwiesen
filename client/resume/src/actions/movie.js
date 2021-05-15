b4ba5df932747a10851853e65965e998ddb4cf46
Sam Li <weisen.li@hotmail.com>
2021-05-15 15:00:00 -0400

    add limit page to getmovies

--- client/resume/src/actions/movie.js
+++ client/resume/src/actions/movie.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-export const getMovies = () => {
+export const getMovies = ({limit,page}) => {
+  var limit = limit || 15;
+  var page = page || 1;
   return dispatch=> {
-    axios.get('https://what-i-watched.herokuapp.com/api/visuals')
+    axios.get(`https://what-i-watched.herokuapp.com/api/visuals?limit=${limit}&page=${page}`)
     .then(ex=>
-      dispatch({type:'GET_MOVIES',ex:ex.data})
+      dispatch({type:'GET_MOVIES',ex:ex.data.results})
     );
   }
 };
