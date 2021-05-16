04ef02ad8bcf3196c45c6889ebf3399a385fd3d8
Sam Li <weisen.li@hotmail.com>
2021-05-16 11:13:09 -0400

    move status bar

--- client/resume/src/Main.jsx
+++ client/resume/src/Main.jsx
@@ -38,8 +38,8 @@ export default class Main extends React.Component {
         <HashRouter key="page">
           <div>
             <BackgroundCanvas />
-            <Nav key="nav" />
             <Status key="status"/>
+            <Nav key="nav" />
             <div id="mainContainer" className="container">
               <Widgets/>
               <main id="main">
