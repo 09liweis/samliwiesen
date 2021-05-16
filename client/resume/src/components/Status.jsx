047d8e3b013b3c099b084af30c131e973f4bf0f1
Sam Li <weisen.li@hotmail.com>
2021-05-16 11:12:50 -0400

    re position status bar

--- client/resume/src/components/Status.jsx
+++ client/resume/src/components/Status.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Status = () => {
+  const [level, setLevel] = useState(0);
+  const [isCharging, setCharging] = useState(false);
+  const getBatteryStatus = () => {
+    var batteryManager = navigator.getBattery;
+    if (typeof batteryManager == 'undefined') {
+      return;
+    }
+    navigator.getBattery().then(function(battery){
+      var {level, charging} = battery;
+      setCharging(charging);
+      setLevel(level);
+      battery.addEventListener('chargingchange',function() {
+        var {charging} = battery;
+        setCharging(charging);
+      });
+      battery.addEventListener('levelchange',function() {
+        var {level} = battery;
+        setLevel(level);
+      });
+    });
+  }
+  getBatteryStatus();
   return(
-    <header id="status"></header>
+    <header id="status">
+      <span>{level}</span>
+      <span>{isCharging?'charging':''}</span>
+    </header>
   );
 }
 export default Status;
