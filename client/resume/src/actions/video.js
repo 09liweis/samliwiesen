b25838bd6f24f82251b755f90186d56b022a89c1
Sam Li <weisen.li@hotmail.com>
2021-05-18 14:22:45 -0400

    call bilibili api in action

--- client/resume/src/actions/video.js
+++ client/resume/src/actions/video.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getVideos = () => {
   return dispatch=> {
-    axios.get(`https://api.bilibili.com/x/space/arc/search?mid=19524235&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`)
+    axios.post('/api/visuals/bilibili')
     .then(ex=>
       dispatch({type:'GET_VIDEOS',ex:ex.data})
     );
