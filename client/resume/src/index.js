045ccb0636f0b4db7da5b4497c863a8792ce54bb
Sam Li <weisen.li@hotmail.com>
2021-05-19 12:25:07 -0400

    import backgroundCanvas

--- client/resume/src/index.js
+++ client/resume/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Main from './Main.jsx';
+import './backgroundCanvas.js';
 
 const root = document.getElementById('root');
 render(<Main />,root);
