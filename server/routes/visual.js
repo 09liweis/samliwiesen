38e2623b995d3cdaa777d67e72899fe33581a5b7
Sam Li <weisen.li@hotmail.com>
2021-05-27 10:03:50 -0400

    add video route

--- server/routes/visual.js
+++ server/routes/visual.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {search,inTheatre,getCelebrities,getSummary,getPhotoDetail,getComments,getReviews} = require('../controllers/visual.js');
 const {getImdbBoxOffice} = require('../controllers/imdb.js');
-const {getSubjects,getTags,getPhotos,getVideos,getCast} = require('../controllers/douban.js');
+const {getSubjects,getTags,getPhotos,getVideos,getVideo,getCast} = require('../controllers/douban.js');
 const {getBilibili} = require('../controllers/bilibili.js');
 const {getMaoyan} = require('../controllers/maoyan.js');
 
@@ -18,6 +18,8 @@ router.route('/cast').post(getCast);
 router.route('/summary').post(getSummary);
 
 router.route('/videos').post(getVideos);
+
+router.route('/video').post(getVideo);
 
 router.route('/photos').post(getPhotos);
 
