0af964ac179866852324d856effc921ed2a52a14
Sam Li <weisen.li@hotmail.com>
2021-06-10 15:10:13 -0400

    remove statusCode from maoyan

--- server/controllers/maoyan.js
+++ server/controllers/maoyan.js
@@ -3,7 +3,7 @@ const {sendRequest, sendResp, sendErr} = require('../helpers/request');
 
 exports.getMaoyan = (req,resp) => {
   const url = 'https://piaofang.maoyan.com/dashboard-ajax';
-  sendRequest({url},(err,{statusCode,body}) => {
+  sendRequest({url},(err,{body}) => {
     try {
       var ret = JSON.parse(body);
     } catch (error) {
