f0f44fa274f920a0eb9fc124d9358e4aceb3cf14
Sam Li <weisen.li@hotmail.com>
2021-06-13 10:50:11 -0400

    refactor getBilibili

--- server/controllers/bilibili.js
+++ server/controllers/bilibili.js
@@ -1,7 +1,8 @@
 const {sendRequest, sendResp, sendErr} = require('../helpers/request');
 
 exports.getBilibili = (req,resp) => {
-  const url = 'https://api.bilibili.com/x/space/arc/search?mid=19524235&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp';
+  const userId = '19524235';
+  const url = `https://api.bilibili.com/x/space/arc/search?mid=${userId}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`;
   sendRequest({url},(err,{body}) => {
     try {
       var ret = JSON.parse(body);
