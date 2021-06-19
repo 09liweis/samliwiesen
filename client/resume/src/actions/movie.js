5dea53cb4e76976cd2f8a05e2531ac7654d2d4fc
Sam Li <weisen.li@hotmail.com>
2021-06-19 08:29:25 +0800

    update what i watched http

--- client/resume/src/actions/movie.js
+++ client/resume/src/actions/movie.js
@@ -4,7 +4,7 @@ export const getMovies = ({limit,page}) => {
   var limit = limit || 15;
   var page = page || 1;
   return dispatch=> {
-    axios.get(`https://what-i-watched.herokuapp.com/api/visuals?limit=${limit}&page=${page}`)
+    axios.get(`http://what-i-watched.herokuapp.com/api/visuals?limit=${limit}&page=${page}`)
     .then(ex=>
       dispatch({type:'GET_MOVIES',ex:ex.data.results})
     );
