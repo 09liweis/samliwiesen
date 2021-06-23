a890a53c0ccf7d8fc2f44d749c34ed2d9e51edf6
Sam Li <weisen.li@hotmail.com>
2021-06-23 23:04:35 +0800

    remove commented code

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,9 +10,4 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.post('/detail',verify, detail);
 
-// router.route('/:id')
-// .get(BlogController.blog_detail)
-// .put(BlogController.blog_update)
-// .delete(BlogController.blog_delete);
-
 module.exports = router;
