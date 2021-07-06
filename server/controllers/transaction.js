1e6746deb884de8c7cd578d507023897ba59cb1d
Sam Li <weisen.li@hotmail.com>
2021-07-06 11:53:57 +0800

    sendResp in detail transaction

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -108,7 +108,7 @@ exports.update = (req, resp) => {
 exports.detail = async function(req, resp) {
   const id = req.params.id;
   const t = await Transaction.findById(id,'title price date category').populate('place', '_id place_id name address lat lng');
-  resp.status(200).json(t);
+  sendResp(resp, t);
 };
 
 exports.remove = (req, resp) => {
